2542d8c58d4c992d4305967a7d7a2a06a9ea3315
Mark Meves <mark.meves@gmail.com>
2010-12-17 07:04:25 -0500

    Add zonky constant (@todo), also wrapper for parsing thing

--- lib/skylab/css-transform/test/testlib.rb
+++ lib/skylab/css-transform/test/testlib.rb
@@ -1,10 +1,32 @@
 require File.expand_path('../../lib/css-convert', __FILE__)
 
+require 'ruby-debug'
+
+Hipe__CssConvert__Testlib = true
+
 module Hipe::CssConvert::ExampleHelperMethods
   def new_cli
     Hipe::CssConvert.cli.buffered!
   end
-  def fixture tail
-    File.join('spec/fixtorros', tail)
+  def fixture_path tail
+    File.join('test/fixtures', tail)
   end
+  def parse_directives_in_file path
+    require Hipe::CssConvert::ROOT + '/grammar/program-parser.rb' # wants wrapper
+    p = Hipe::CssConvert::Grammar::ProgramParser.new
+    contents = File.read(path)
+    puts contents if @debuggy
+    resp = p.parse contents
+    if ! resp.nil?
+      resp.tree
+    elsif @debuggy
+      puts "got failure:"
+      puts p.failure_reason
+      nil
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include Hipe::CssConvert::ExampleHelperMethods
 end
