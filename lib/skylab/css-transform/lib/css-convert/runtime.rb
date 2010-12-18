6d71569e630edb25570cfeb140a3ab62e698b392
Mark Meves <mark.meves@gmail.com>
2010-12-17 22:18:30 -0500

    Add note at other parser

--- lib/skylab/css-transform/lib/css-convert/runtime.rb
+++ lib/skylab/css-transform/lib/css-convert/runtime.rb
@@ -6,7 +6,9 @@ module Hipe; end
 class Treetop::Runtime::CompiledParser
   #
   # careful! we monkeypatch this one b/c we don't like not having quotes around strings.
+  # @todo move this down to etc as we did for the other
   #
+
   def failure_reason
     return nil unless (tf = terminal_failures) && tf.size > 0
     "Expected " +
