fc8915dacfccb6b7600f1d623182e59fd633ce04
Mark Meves <mark.meves@gmail.com>
2010-12-29 07:48:57 -0500

    Remove node classes this will change on the refactor

--- lib/skylab/css-transform/lib/css-convert/node-classes.rb
+++
@@ -1,9 +0,0 @@
-module Hipe
-  module CssConvert
-    module Grammar
-      # class Node < Treetop::Runtime::SyntaxNode end
-      S = ::Hipe::CssConvert::Sexpie # shorthand!
-    end
-  end
-end
-      
