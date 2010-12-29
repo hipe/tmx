6e7b3f76e272e08947264be0fb1145ebb0f1efdc
Mark Meves <mark.meves@gmail.com>
2010-12-28 23:16:29 -0500

    Here's a lockdown showing r+ o+ r+ grammar
    (optionals in the middle)
    
    this would be like a function arguments looking like:
      def foo(a, b, *c, d, e)

--- lib/skylab/css-transform/lib/css-convert/core-interface.rb
+++ lib/skylab/css-transform/lib/css-convert/core-interface.rb
@@ -2,12 +2,19 @@ module Hipe::CssConvert
   require ROOT + '/interface-reflector'
   class CoreInterface
     extend InterfaceReflector
-    class << self
-      def build_interface
-        RequestParser.new do |o|
-          o.on('-v', '--version', 'Display version information.')
-          o.on('-h', '--help',    'Display help screen.'        )
-        end
+    def self.build_interface
+      InterfaceReflector::RequestParser.new do |o|
+        o.on('-d', '--directives',
+          '(debugging) Show sexp of parsed directives file.')
+        o.on('-v', '--version', 'Display version information.')
+        o.on('-h', '--help',    'Display help screen.'        )
+
+        o.arg('<first-req>', 'A file with directives in it.')
+        o.arg('<sec-req>', 'Another arg')
+        o.arg('[<third-req>]', 'Another arg')
+        o.arg('[<fourth-req>]', 'Another arg')
+        o.arg('[<fifth-req>]', 'Another arg')
+        o.arg('<sixth-req>', 'Another arg')
       end
     end
     def build_context
@@ -17,9 +24,13 @@ module Hipe::CssConvert
       require ROOT + '/version'
       VERSION
     end
+    def on_directives
+      @exit_ok = true
+      @c[:show_parsed_directives] = true
+    end
     def run_convert
       @exit_ok and return
-      @c.out.puts "ok whatever: #{@argv.inspect}"
+      @c.out.puts "ok whatever: #{@c.inspect}"
     end
   end
 end
