437e2669779769aac783f567336eabdeb29f05f6
Mark Meves <mark.meves@gmail.com>
2010-12-30 11:21:06 -0500

    start to use treetop tools

--- lib/skylab/css-transform/lib/css-convert/core-interface.rb
+++ lib/skylab/css-transform/lib/css-convert/core-interface.rb
@@ -4,8 +4,9 @@ module Hipe::CssConvert
     extend InterfaceReflector
     def self.build_interface
       InterfaceReflector::RequestParser.new do |o|
+        o.on('-f', '--force', 'overwrite existing generated grammars')
         o.on('-d', '--directives',
-          '(debugging) Show sexp of parsed directives file.')
+          '(debugging) Show sexp of parsed directives file. (twice exits)')
         o.on('-v', '--version', 'Display version information.')
         o.on('-h', '--help',    'Display help screen.'        )
         o.arg('<directives-file>', 'A file with directives in it.')
@@ -15,8 +16,11 @@ module Hipe::CssConvert
     def default_action; :run_convert           end
     def build_context
       c = ExecutionContext.new
-      c[:tmpdir] = ROOT + '/tmp'
+      c[:tmpdir_relative] = "../../tmp"
       c
+    end
+    def on_force
+      @c[:force_overwrite] = true
     end
     def on_version
       require ROOT + '/version'
@@ -24,25 +28,27 @@ module Hipe::CssConvert
       @exit_ok = true
     end
     def on_directives
-      @c[:show_parsed_directives] = true
+      @c[ @c.key?(:show_parsed_directives) ?
+        :show_parsed_directives_and_exit : :show_parsed_directives ] = true
     end
     def run_convert
       sexp = parse_directives_in_file(@c[:directives_file]) or return
       if @c[:show_parsed_directives]
         require 'pp'
         PP.pp(sexp, @c.err)
-        return
+        @c[:show_parsed_directives_and_exit] and return
       end
       @c.out.puts "ok whatever"
     end
     def parse_directives_in_file path
       require ROOT + '/directives-parser'
+      sexp =
       begin
         DirectivesParser.new(@c).parse_file(path)
-      rescue DirectivesParser::RuntimeError => e
+      rescue DirectivesParser::RuntimeError, TreetopTools::RuntimeError => e
         return error(style('error: ', :error) << e.message)
       end
+      @c.out.puts("wow, got: #{sexp.inspect}")
     end
   end
 end
-
--- lib/skylab/css-transform/lib/css-convert/directives-parser.rb
+++ lib/skylab/css-transform/lib/css-convert/directives-parser.rb
@@ -1,15 +1,60 @@
 class Hipe::CssConvert::DirectivesParser
+  ROOT = ::Hipe::CssConvert::ROOT
   class RuntimeError < ::RuntimeError; end
+  class << self
+    def require_local_treetop
+      Object.const_defined?(:Treetop) and return
+      require 'rubygems' # still use ployglot as found by rubygems
+      $:.unshift(File.dirname(File.dirname(ROOT))+'/vendor/lib/treetop/lib')
+        # use treetop in vendor/lib, not the one in
+        # rubygems/rvm/gemspec/whatever, in case etc.. @todo
+      require 'treetop'
+    end
+  end
   def initialize ctx
     @c = ctx
   end
   def parse_file path
     path && File.exist?(path) or
       return error("directives file not found: #{path.inspect}")
-    { :foo => 'bar' }
+    parse_string File.read(path)
+  end
+  def parse_string whole_string
+    resp = parser.parse(whole_string)
+    if resp.nil?
+      rsn = parser.failure_reason || "Got nil from parse without reason!"
+      @c.err.puts rsn
+      nil
+    else
+      [:fake, :sexp]
+      # sexp = resp.sexp
+      # @c.key?(:dump_sexp) and PP.pp(sexp, @c.out)
+      # @c.key?(:exit_after_dump) and return
+      # @c.key?(:progressive) and @c.builder.progressive_output!
+      # sexp.translate(@c)
+      # @c.key?(:progressive) or @c.out.write(@c.builder.ruby)
+    end
   end
 private
   def error msg
     raise RuntimeError.new(msg)
+  end
+  def parser_class
+    @parser_class ||= begin
+      require ROOT + '/treetop-tools'
+      self.class.require_local_treetop
+      Hipe::CssConvert::TreetopTools.load_parser_class do |o|
+        o.root_dir ROOT
+        o.generated_grammar_dir @c[:tmpdir_relative]
+        o.treetop_grammar 'directives-parser/common.treetop'
+        o.treetop_grammar 'directives-parser/directives.treetop'
+        o.force_overwrite! if @c[:force_overwrite]
+        o.enhance_parser_with ::Hipe::CssConvert::TreetopTools::ParserExtlib
+        o.use_ui @c
+      end
+    end
+  end
+  def parser
+    @parser ||= parser_class.new
   end
 end
