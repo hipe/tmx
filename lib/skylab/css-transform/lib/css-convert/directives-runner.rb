6aa4e5e7b9f6d5b5959f6c3f63457ceb16a90b40
Mark Meves <mark.meves@gmail.com>
2010-12-30 13:41:11 -0500

    Directives runner about ready to parse css

--- lib/skylab/css-transform/lib/css-convert/directives-runner.rb
+++ lib/skylab/css-transform/lib/css-convert/directives-runner.rb
@@ -1,8 +1,37 @@
-class Hipe::CssConvert::DirectivesRunner
-  def initialize(ctx)
-    @c = ctx
+module Hipe::CssConvert
+  class DirectivesRunner
+    def initialize(ctx)
+      @c = ctx
+    end
+    def run directives_sexp
+      mod = self.class.to_s.match(/\A(.+[^:])::[^:]+\Z/)[1]
+      cls = directives_sexp.node_name.to_s.gsub(/([a-z])_([a-z])/) do
+        "#{$1}#{$2.upcase}" end.sub(/\A([a-z])/){ $1.upcase }
+      c = mod.split('::').inject(Object){|m,n| m.const_get(n)}.const_get(cls)
+      c.new(@c, directives_sexp).run
+    end
   end
-  def run directives_sexp
-    @c.out.puts "ok running haha yeah right"
+  class MergeStatement
+    def initialize ctx, sexp
+      @c = ctx
+      @sexp = sexp
+    end
+    def run
+      left, right = @sexp[:styles_in_files].cx_slice(:left, :right)
+      if @sexp[:in_the_folder]
+        p = @sexp[:in_the_folder][:path]
+        [left, right].each { |x| x.replace("#{p}/#{x}") }
+      end
+      # lp = css_parser.parse_file(left)
+      # rp = css_parser.parse_file(right)
+      @c.out.puts("skipping this for now in directives runner")
+    end
+  private
+    def css_parser
+      @css_parser ||= begin
+        require ROOT + '/css-parser'
+        CssParser.new(@c)
+      end
+    end
   end
 end
