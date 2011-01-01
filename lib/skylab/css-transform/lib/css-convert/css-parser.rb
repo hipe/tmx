3914691d0337de525e2bd71e642c941b2663d2bc
Mark Meves <mark.meves@gmail.com>
2010-12-31 19:48:58 -0500

    just being cute make these oneliners

--- lib/skylab/css-transform/lib/css-convert/css-parser.rb
+++ lib/skylab/css-transform/lib/css-convert/css-parser.rb
@@ -13,23 +13,25 @@ module Hipe::CssConvert
       f = @c[:force_overwrite]
       v = @c[:verbose]
       parsers = "#{ROOT}/#{@c[:tmpdir_relative]}"
+      docu = "#{parsers}/css2.1.yacc3wc"
       toks = "#{parsers}/css-tokens.treetop.rb"
       sels = "#{parsers}/css-selectors.treetop.rb"
       f || v || !File.exist?(toks) and build_tokens_parser(toks)   || return
       f || v || !File.exist?(sels) and build_selector_parser(sels) || return
     end
     def build_tokens_parser path
-      ftt = flex_to_treetop::Translator.new(@c.merge(:root => ROOT))
-      ftt.execution_context[:grammar] = "Hipe::CssParser::Tokens"
-      opts = { :force => @c[:force_overwrite] }
-      ftt.translate(ROOT + '/css-parser/tokens.flex', path, opts)
-      true
+      flex_to_treetop::Translator.new(@c.merge(:root => ROOT)).
+        translate(ROOT + '/css-parser/tokens.flex', path,
+          :force   => @c[:force_overwrite],
+          :grammar => "Hipe::CssParser::Tokens"
+        )
     end
     def build_selector_parser path
-      ytt = yacc_to_treetop::Translator.new(@c.merge(:root => ROOT))
-      ytt.execution_context[:grammar] = "Hipe::CssParser::Selectors"
-      opts = { :force => @c[:force_overwrite] }
-      ytt.translate(ROOT + '/css-parser/selectors.yaccw3c', path, opts)
+      yacc_to_treetop::Translator.new(@c.merge(:root => ROOT)).
+        translate(ROOT + '/css-parser/selectors.yaccw3c', path,
+          :force   => @c[:force_overwrite],
+          :grammar => "Hipe::CssParser::Selectors"
+        )
     end
     def flex_to_treetop
       ::Hipe.const_defined?(:FlexToTreetop) or
