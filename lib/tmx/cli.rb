2368d539c6b5962a95777834035786465d6f01d3
Mark Meves <mark.meves@gmail.com>
2011-08-08 01:09:17 -0400

    here is stuff for the version pseudo command.

--- lib/tmx/cli.rb
+++ lib/tmx/cli.rb
@@ -2,14 +2,21 @@ o = File.dirname(__FILE__)
 require o + '/face/cli.rb'
 
 module Skylab; end
-module Skylab::Tmx; end
-module Skylab::Tmx::Modules; end
 
-class Skylab::Tmx::Cli < Skylab::Face::Cli
+module Skylab::Tmx
+
+  module Modules; end
+
   Face = Skylab::Face
-  Dir["#{File.dirname(__FILE__)}/modules/*/cli.rb"].each do |cli_path|
-    len = Face::Command::Namespace.namespaces.length
-    require cli_path
-    namespace Face::Command::Namespace.namespaces[len]
+
+  class Cli < Face::Cli
+
+    version { require "#{File.dirname(__FILE__)}/version"; VERSION }
+
+    Dir["#{File.dirname(__FILE__)}/modules/*/cli.rb"].each do |cli_path|
+      len = Face::Command::Namespace.namespaces.length
+      require cli_path
+      namespace Face::Command::Namespace.namespaces[len]
+    end
   end
 end
---
+++ lib/tmx/version.rb
@@ -0,0 +1,5 @@
+module Skylab
+  module Tmx
+    VERSION = "0.0.x"
+  end
+end
