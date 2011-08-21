e36c260701046e77043cd7789cd3635fd5a6d436
Mark Meves <mark.meves@gmail.com>
2011-08-20 22:54:21 -0400

    [#002] finish refactor of path tools

--- lib/skylab/face/path-tools.rb
+++ lib/skylab/face/path-tools.rb
@@ -1,9 +1,13 @@
 require 'fileutils'
 
-module Skylab::Tmx::Modules::Cli::PrettyPath
-  HomeDirRe = /\A#{Regexp.escape(ENV['HOME'])}/
-protected
-  def pretty_path path
-    path.sub(/\A#{Regexp.escape(FileUtils.pwd)}\//, './').sub(HomeDirRe, '~')
+module Skylab; end
+
+module Skylab::Face
+  module PathTools
+    extend self
+    HomeDirRe = %r{\A#{Regexp.escape(ENV['HOME'])}/}
+    def pretty_path path
+      path.sub(/\A#{Regexp.escape(FileUtils.pwd)}\//, './').sub(HomeDirRe, '~/')
+    end
   end
 end
