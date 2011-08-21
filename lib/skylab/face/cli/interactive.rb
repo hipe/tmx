7f751039d117b771528f586718189da467290a0d
Mark Meves <mark.meves@gmail.com>
2011-08-20 22:45:48 -0400

    [#002] here's a way you can make your things optionally interactive

---
+++ lib/skylab/face/cli/interactive.rb
@@ -0,0 +1,32 @@
+class Skylab::Face::Cli
+  class Interactive
+    def self.run ui, opts, request
+      new(ui, opts, request).run
+    end
+    def initialize ui, opts, request
+      @ui, @opts, @request = [ui, opts, request]
+    end
+    def run
+      require 'highline'
+      @terminal = ::HighLine.new($stdin, @ui.err)
+      @params = @request.parameters
+      @request.label and @terminal.say(@request.label)
+      @request.description and @terminal.say(@request.description)
+      begin
+        @params.each do |param|
+          unless ask_for(param)
+            break;
+          end
+        end
+      rescue ::Interrupt => e
+        @terminal.say("\nCancelled #{@request.some_label.downcase}.")
+        @request.valid = false
+      end
+      @request
+    end
+    def ask_for param
+      value = @terminal.ask("#{param.label}: ")
+      param.set_response(value)
+    end
+  end
+end
