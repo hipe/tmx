820a6bc05c992968863bff958bb5a27f0e24b20c
Mark Meves <mark.meves@gmail.com>
2011-08-22 01:43:45 -0400

    [#now] add json-file to code-molester

---
+++ lib/skylab/code-molester/json-file.rb
@@ -0,0 +1,54 @@
+require 'json'
+require File.expand_path('../../face/path-tools', __FILE__)
+
+module Skylab; end
+
+module Skylab::CodeMolester
+  class JsonFile
+    def initialize path
+      @path = path
+      if File.exist?(@path)
+        _data = nil
+        begin
+          _data = JSON.parse(File.read(@path))
+          @data = _data
+        rescue JSON::ParserError => e
+          @last_parser_error = e
+        end
+      else
+        @data = nil
+      end
+    end
+    attr_reader :path
+    def data
+      valid? or fail("Cannot request data from an invalid json file: #{@last_validation_error}")
+      @data
+    end
+    def exists?
+      File.exist?(@path)
+    end
+    def pretty_path
+      Skylab::Face::PathTools.pretty_path(@path)
+    end
+    def write
+      bytes = nil
+      str = @data.to_json
+      len = str.length
+      len == 0 and return 0 # don't create empty files (for now)
+      str =~ /\n\Z/ or str = "#{str}\n" # add newline to end of file if necessary
+      File.open(@path, 'w+') do |fh|
+        fh.write str
+        bytes = len
+      end
+      bytes
+    end
+    def valid?
+      if @last_parser_error
+        @last_validation_error = @last_parse_error.to_s
+        return false
+      end
+      true # for now
+    end
+    attr_reader :last_validation_error
+  end
+end
