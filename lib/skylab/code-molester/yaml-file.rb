14db1b3ff9a276d30433381fcc057ed42c3ead28
Mark Meves <mark.meves@gmail.com>
2011-08-21 20:48:23 -0400

    [#now] total overhaul of yaml parsing

--- lib/skylab/code-molester/yaml-file.rb
+++ lib/skylab/code-molester/yaml-file.rb
@@ -4,128 +4,142 @@ module Skylab; end
 
 module Skylab::CodeMolester
   module Yaml
-    module ModuleMethods
-      def syck_to_node foo
-        tree = Tree.new
-        foo.value.each do |key, val|
-          if :map == val.kind
-            subtree = syck_to_node val
+    class Node
+      def initialize
+      end
+      # because of some facepalming decisions of how yaml trees are represented in psych, this.
+      # this has some facepalming vulnerabilities of its own
+      def init_from_psych_tree psych
+        tree? or _change_to_tree!
+        key = nil
+        i = -1 ; length = psych.children.length ; while (i+=1) < length
+          child = psych.children[i]
+          if child.children
+            node = Node.new
+            node.init_from_psych_tree child
+            _set_child key, node
+            key = nil
+          elsif key
+            node = Node.new
+            node.value = child.value
+            _set_child key, node
+            key = nil
           else
-            tree[key.value] = val.value
+            key = child.value
           end
         end
-        tree
+        nil
       end
-    end
-    class ValueNode
-      def initialize key, value
-        @key = key.intern
-        @value = value
-      end
-      attr_reader :key
-      attr_accessor :value
       def leaf?
-        true
+        @children_hash.nil?
       end
-    end
-    module BufferAdapter
-      def self.[] str
-        str.extend self
-        str
+      def tree?
+        ! @children_hash.nil?
       end
-      def puts str
-        str ||= ''
-        str =~ /\n\Z/ or str = "#{str}\n"
-        concat str
+      def change_to_tree!
+        @value.nil? or @value = nil # don't create attribute if not set
+        @ordered_keys = []
+        @children_hash = {}
       end
-    end
-    class Tree
-      def initialize name = nil
-        @key = name ? name.intern : nil
-        @children = []
+      def key? key
+        tree? or return false
+        @children_hash.key? key.intern
       end
-      def []= (k, v)
-        k = k.intern
-        if index = @children.index { |c| c.key == k }
-          node = @children[index]
-          if node.branch?
-            node.value = v
-          else
-            @children[index] = ValueNode.new(k, v)
-          end
+      def []= k, v
+        node!(k).value = v
+      end
+      def [] k
+        tree? or return nil
+        @children_hash[k]
+      end
+      def value= val
+        leaf? or fail("won't demote tree to leaf (for now)")
+        @value = val
+      end
+      attr_reader :value
+      def node! key
+        tree? or _change_to_tree!
+        if @children_hash.key?(key = key.intern)
+          @children_hash[key]
         else
-          @children.push ValueNode.new(k, v)
+          @ordered_keys.push key
+          @children_hash[key] = Node.new
         end
       end
-      def leaf?
-        false
-      end
-      def node! name
-        key = name.intern
-        unless child = @children.detect { |c| c.key == key }
-          child = Tree.new(name)
-          @children.push(child)
+      def _set_child key, val
+        unless @ordered_keys.include?(key = key.intern)
+          @ordered_keys.push key
         end
-        child
+        @children_hash[key] = val
       end
+      private :_set_child
       def to_string margin=nil, opts=nil
         if opts.nil? && margin.kind_of?(Hash)
           opts = margin
-          margin = ''
+          margin = nil
         end
         opts ||= {}
-        opts[:indent_with] ||= '  '
-        opts[:buffer] ||= BufferAdapter['']
-        out = opts[:buffer]
-        if @key
-          out.puts "#{margin}#{@key}:"
-          child_margin = "#{margin}#{opts[:indent_with]}"
+        opts[:buffer] ||= ''.extend(BufferAdapter)
+        if leaf?
+          opts[:buffer].write(@value.to_s)
         else
-          child_margin = margin
-        end
-        @children.each do |node|
-          if node.kind_of?(ValueNode)
-            out.puts "#{child_margin}#{node.key}: #{node.value}"
-          else
-            node.to_string child_margin, opts
+          opts[:indent_with] ||= '  '
+          margin ||= ''
+          child_margin = "#{margin}#{opts[:indent_with]}"
+          newline = nil
+          @ordered_keys.each do |key|
+            child = @children_hash[key]
+            sep = child.leaf? ? ' ' : "\n"
+            opts[:buffer].write "#{newline}#{margin}#{key}:#{sep}"
+            newline ||= "\n"
+            child.to_string(child_margin, opts)
           end
         end
-        out
+        opts[:buffer]
+      end
+    private
+      def _change_to_tree!
+        @value.nil? or fail("won't change node to tree when a value is present!")
+        change_to_tree!
       end
     end
   end
-  class YamlFile
-    extend Yaml::ModuleMethods
+  module BufferAdapter
+    def puts str
+      str ||= ''
+      str =~ /\n\Z/ or str = "#{str}\n"
+      concat str
+    end
+    def write str
+      concat str
+    end
+  end
+
+  class YamlFile < Yaml::Node
     def initialize path
       @path = path
       if File.exist?(@path)
         parsed = YAML.parse(File.read(@path))
         if false == parsed # empty file, special case
-          @node = Yaml::Tree.new
+          # nothing for now
         else
-          @node = self.class.syck_to_node(parsed)
+          init_from_psych_tree parsed
         end
-      else
-        @node = Yaml::Tree.new
       end
     end
     attr_reader :path
     def exists?
       File.exist?(@path)
     end
-    def node! name
-      @node.node! name
-    end
     def pretty_path
       Skylab::Face::PathTools.pretty_path(@path)
     end
-    def to_s
-      @node.to_string
-    end
     def write
       bytes = nil
-      str = @node.to_string(:indent_with => '  ')
+      str = to_string(:indent_with => '  ')
       len = str.length
+      len == 0 and return 0 # don't create empty files (for now)
+      str =~ /\n\Z/ or str = "#{str}\n" # add newline to end of file if necessary
       File.open(@path, 'w+') do |fh|
         fh.write str
         bytes = len
