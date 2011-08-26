64b0ece0dd045c5f82435a50f1db641f6901a76b
Mark Meves <mark.meves@gmail.com>
2011-08-25 23:02:26 -0400

    [#004] here is a task that makes a symlink to a file

---
+++ lib/skylab/face/dependency-graph/task-types/symlink.rb
@@ -0,0 +1,40 @@
+require File.expand_path('../../task', __FILE__)
+module Skylab::Face
+  class DependencyGraph
+    class TaskTypes::Symlink < Task
+      attribute :symlink
+      attribute :target
+      def slake
+        interpolated? or interpolate! or return false
+        stay, success = check
+        stay or return success
+        deps? or return dead_end
+        slake_deps or return false
+        _, success = check
+        success
+      end
+      def check
+        if File.exist?(target)
+          if File.exist?(symlink)
+            if File.lstat(symlink).symlink?
+              [false, true] # later for checking
+            else
+              @ui && @ui.err.puts("#{hi_name}: File exists but is not symlink: #{symlink}")
+              [false, false]
+            end
+          else
+            @ui && @ui.err.puts("#{hi_name}: ln -s #{target} #{symlink}")
+            if 0 == (r = File.symlink(target, symlink))
+              [false, true]
+            else
+              @ui && @ui.err.puts("#{hi_name}: Unexpected status from symlink command: #{r.inspect}")
+              [false, false]
+            end
+          end
+        else
+          [true, nil] # the target file does not exist.  stay and try this again
+        end
+      end
+    end
+  end
+end
