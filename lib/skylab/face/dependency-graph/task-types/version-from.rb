f768cd21b874f2b20b20bc1aab5755bb643c1af7
Mark Meves <mark.meves@gmail.com>
2011-08-26 04:35:06 -0400

    [#004] version task does assertion as well as reporting now

--- lib/skylab/face/dependency-graph/task-types/version-from.rb
+++ lib/skylab/face/dependency-graph/task-types/version-from.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../../task', __FILE__)
+require File.expand_path('../../version-range', __FILE__)
 require 'stringio'
 
 module Skylab::Face
@@ -7,22 +8,40 @@ module Skylab::Face
 
       attribute :version_from
       attribute :parse_with, :required => false
+      attribute :must_be_in_range, :required => false
+      attribute :presupposes, :required => false # experimental, must be pushed up if stays
 
       include Open2
 
       def run
-        @parse_with and @regex = build_regex(@parse_with)
-        buffer = StringIO.new
-        read = lambda { |s| buffer.write(s) }
-        open2(version_from) { |on| on.out(&read); on.err(&read) }
-        str = buffer.rewind && buffer.read
-        if @regex and @regex =~ str
-          @ui.err.puts "  #{hi('version:')} #{$1}"
+        version, used_regex = parse_version_string
+        if used_regex
+          @ui.err.puts "  #{hi('version:')} #{version}"
         else
-          @ui.err.puts str.split("\n").map { |s| "  #{hi('version')}: #{s}" }
+          @ui.err.puts version.split("\n").map { |s| "  #{hi('version')}: #{s}" }
         end
         @ui.err.puts "#{hi_name}: #{@version_from}"
       end
+
+      def check_presuppositions
+        @presupposes or return true
+        @graph.node(@presupposes).check
+      end
+
+      def check
+        check_presuppositions or return false
+        version_range = build_version_range
+        version_string = get_version_string
+        if version_range.match(version_string)
+          @ui.err.puts("  #{hi 'version ok'}: version #{version_string} is in range #{version_range}")
+          true
+        else
+          @ui.err.puts("  #{ohno 'version mismatch'}: needed #{version_range} had #{version_string}")
+          false
+        end
+      end
+
+      alias_method :slake, :check # this is a check-only task, so they are the same
 
       RegexpRegexp = %r{\A/(.+)/([a-z]*)\z}
       ModifierRe = /\A[imox]*\z/
@@ -36,6 +55,31 @@ module Skylab::Face
         else
           fail("Failed to parse regexp: #{str.inspect}.  Needed #{RegexpRegexp.source}")
         end
+      end
+
+      def get_version_string
+        parse_version_string.first
+      end
+
+      def parse_version_string
+        @parse_with and @regex = build_regex(@parse_with)
+        buffer = StringIO.new
+        read = lambda { |s| buffer.write(s) }
+        open2(version_from) { |on| on.out(&read); on.err(&read) }
+        str = buffer.rewind && buffer.read
+        if @regex and @regex =~ str
+          [$1, true]
+        else
+          [str, false]
+        end
+      end
+
+      def build_version_range
+        @must_be_in_range or fail(<<-HERE.gsub(/\n */,' ').strip
+          Do not use "version from" as a target without a "must be in range" assertion.
+        HERE
+        )
+        VersionRange.build(@must_be_in_range)
       end
     end
   end
