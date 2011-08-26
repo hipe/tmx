44126985aa1ef14a8c9cf3d155b2a71d36ec4f4a
Mark Meves <mark.meves@gmail.com>
2011-08-26 06:19:33 -0400

    [#004] for attributes thing sketchily allow inheritable attributes

--- lib/skylab/face/dependency-graph/task.rb
+++ lib/skylab/face/dependency-graph/task.rb
@@ -14,15 +14,41 @@ module Skylab::Face
           task
         end
         def attribute sym, opts={}
-          @attributes ||= {}
+          @attributes ||= (dup_parent_attributes || {})
           @attributes[sym] ||= begin
             attr_accessor sym
             { :required => true }
           end
           @attributes[sym].merge!(opts)
         end
-        attr_reader :attributes
+        def attributes
+          unless @attributes
+            @did_dup_parent_attributes ||= begin
+              @attributes = dup_parent_attributes
+              true
+            end
+          end
+          @attributes
+        end
+        def dup_parent_attributes
+          idx = 1
+          parent_attributes = nil
+          loop do
+            case ancestors[idx]
+            when NilClass
+              break 2
+            when Class
+              parent_attributes = ancestors[idx].respond_to?(:attributes) ? ancestors[idx].attributes : nil
+              break 2
+            end
+            idx += 1
+          end
+          parent_attributes ? parent_attributes.dup : {}
+        end
       end
+
+      attribute :enabled, :required => false
+
       def initialize graph
         @graph = graph
         @ui = @graph.ui
@@ -33,6 +59,9 @@ module Skylab::Face
         data.each do |k, v|
           send("#{k.gsub(' ','_')}=", v)
         end
+      end
+      def disabled?
+        ! (@enabled.nil? || @enabled)
       end
       def valid?
         if (missing = self.class.attributes.each.select do |k, v|
