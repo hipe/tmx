e9e5e3ac95a113a626aa19b92e627116f01eb3a4
Mark Meves <mark.meves@gmail.com>
2011-08-31 20:22:09 -0400

    [#now] add code-molester ssh-config.rb

---
+++ lib/skylab/code-molester/ssh-config.rb
@@ -0,0 +1,54 @@
+require File.expand_path('../../face/path-tools', __FILE__)
+require 'strscan'
+
+module Skylab; end
+
+module Skylab::CodeMolester
+  class SshConfig
+    def initialize path
+      @path = path
+      @valid = @data = @invalid_reason = nil
+      if File.exist?(@path)
+        @data = _parse(File.read(@path)) and @valid = true
+      end
+    end
+    attr_reader :path
+    attr_reader :valid
+    alias_method :valid?, :valid
+    attr_reader :invalid_reason
+    def data
+      false == @valid and fail("Cannot request data for an invalid file: #{@invalid_reason.inspect}")
+      @data
+    end
+    def exists?
+      File.exist?(@path)
+    end
+    def pretty_path
+      Skylab::Face::PathTools.pretty_path(@path)
+    end
+    def _parse string
+      hosts = []
+      scn = StringScanner.new(string)
+      scn.skip(/[[:space:]]+/)
+      loop do
+      ok = scn.scan(/Host  */) or return _fail("expected: \"Host\" had: #{scn.peek(20).inspect}")
+        host = { :type => :host }
+        name = scn.scan(/[-a-zA-Z0-9_]+/) or return _fail("expected a valid name, had: #{scn.peek(20).inspect}")
+        scn.skip(/[ \t]+/)
+        while line = scn.scan(/\n? +[A-Za-z]+ +[^\n]+/)
+          name, value = line.strip.split(/ +/)
+          host[name.gsub(/([a-z])([A-Z])/){ "#{$1}_#{$2}" }.downcase.intern] = value
+        end
+        hosts.push host
+        scn.skip(/[[:space:]]+/)
+        scn.eos? and break
+      end
+      hosts
+    end
+    def _fail msg
+      @valid = false
+      @invalid_reason = msg || "Parsing failed (reason unknown)."
+      false
+    end
+  end
+end
