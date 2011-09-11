aed86be1e4fb362ff0ff96d8af428ae142b6dedc
Mark Meves <mark.meves@gmail.com>
2011-09-11 19:25:23 -0400

    [#009] add primordial, a definition structure unparsed

---
+++ lib/skylab/face/dependency-graph/primordial.rb
@@ -0,0 +1,40 @@
+module Skylab
+  module Face
+    class DependencyGraph
+      class Primordial
+        def merge_in! *a, &b
+          @defs ||= []
+          if a.any?
+            case a.size
+            when 1; @defs.push [:file, a[0]]
+            else raise ArgumentError.new("expecting one arg, had #{a.size}")
+            end
+          end
+          b and b.call(self)
+          self
+        end
+        def inflated?
+          false
+        end
+        def inflate
+          require File.expand_path('../../dependency-graph', __FILE__)
+          graph = nil
+          @defs.each do |means, data|
+            _graph =
+            case means
+            when :file
+              Face::DependencyGraph.build_from_file(data)
+            else fail("implement me: #{means}")
+            end
+            if graph
+              graph.merge_in!(_graph) # not implemented!
+            else
+              graph = _graph
+            end
+          end
+          graph
+        end
+      end
+    end
+  end
+end
