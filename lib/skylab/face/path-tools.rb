8baaafa40847282fbba7a36ee3d3b174e0d2d02f
Mark Meves <mark.meves@gmail.com>
2011-09-11 19:30:24 -0400

    [#009] path tools gets beautify_path from elsewhere (needs refactor with pretty_path @todo)

--- lib/skylab/face/path-tools.rb
+++ lib/skylab/face/path-tools.rb
@@ -6,6 +6,11 @@ module Skylab; end
 module Skylab::Face
   module PathTools
     extend self
+    def beautify_path path
+      path.sub(/\A#{
+        Regexp.escape(FileUtils.pwd.sub(/\A\/private\//, '/'))
+      }/, '.')
+    end
     HomeDirRe = %r{\A#{Regexp.escape(ENV['HOME'])}/}
     def pretty_path path
       path.sub(/\A#{Regexp.escape(FileUtils.pwd)}\//, './').sub(HomeDirRe, '~/')
