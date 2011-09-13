563930c7b8b725df820a362e2cd723fba97c1ef8
Mark Meves <mark.meves@gmail.com>
2011-09-13 19:23:05 -0400

    [#010] let's name every dependency document as 'deps.json' even though we don't have to

--- lib/skylab/tmx/modules/schema/cli.rb
+++ lib/skylab/tmx/modules/schema/cli.rb
@@ -3,7 +3,7 @@ require 'skylab/face/cli/external-dependencies'
 module Skylab::Tmx::Modules::Schema
   class Cli < Skylab::Face::Cli
     namespace :schema do
-      external_dependencies "#{File.dirname(__FILE__)}/data/external-dependencies.json"
+      external_dependencies "#{File.dirname(__FILE__)}/data/deps.json"
     end
   end
 end
