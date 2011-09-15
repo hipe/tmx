191e84208094c72f92148833d672153e3a34cbca
Mark Meves <mark.meves@gmail.com>
2011-09-14 23:56:18 -0400

    [#011] do some clever with constants

--- lib/skylab/dependency/test/support/constants.rb
+++ lib/skylab/dependency/test/support/constants.rb
@@ -5,6 +5,7 @@ module Skylab
         TEST_ROOT_DIR = File.expand_path('../..', __FILE__)
         FIXTURES_DIR = "#{TEST_ROOT_DIR}/fixtures"
         TMP_DIR = "#{TEST_ROOT_DIR}/tmp"
+        TEST_BUILD_DIR = File.join(TMP_DIR, 'build_dir')
       end
     end
   end
--- lib/skylab/dependency/test/task-types/get_spec.rb
+++ lib/skylab/dependency/test/task-types/get_spec.rb
@@ -2,19 +2,17 @@ require File.expand_path('../helper', __FILE__)
 require File.expand_path('../../../graph', __FILE__)
 
 module Skylab::Dependency
+  include Test::Support
   describe Graph do
     it "should work" do
-      build_dir = File.join(Test::Support::TEST_ROOT_DIR, 'tmp/build_dir')
-      if File.exist?(build_dir)
-        FileUtils.rm_rf(build_dir, :verbose => true)
-      end
-      FileUtils.mkdir(build_dir, :verbose => true)
+      File.exist?(TEST_BUILD_DIR) and FileUtils.rm_rf(TEST_BUILD_DIR, :verbose => true)
+      FileUtils.mkdir(TEST_BUILD_DIR, :verbose => true)
       Test::Support::StaticFileServer.start_unless_running
       graph = Graph.from_file(File.join(Test::Support::FIXTURES_DIR, 'depz1.json'))
       ui1 = Test::Support::UiTee.new
       ui2 = Test::Support::UiTee.new
-      graph.run(ui1, { :build_dir => build_dir })
-      graph.run(ui2, { :build_dir => build_dir })
+      graph.run(ui1, { :build_dir => TEST_BUILD_DIR })
+      graph.run(ui2, { :build_dir => TEST_BUILD_DIR })
       ui1.out[:buffer].to_str.should eq('')
       ui2.out[:buffer].to_str.should eq('')
       ui1.err[:buffer].to_str.should match(/read 419 bytes/)
