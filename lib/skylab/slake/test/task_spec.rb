d8a93885e64618e7099aa6bf664ef03905260763
Mark Meves <mark.meves@gmail.com>
2011-09-14 21:45:09 -0400

    [#010] lockdown slake/spec with a basic GREEN test

--- lib/skylab/slake/test/task_spec.rb
+++ lib/skylab/slake/test/task_spec.rb
@@ -3,8 +3,12 @@ require File.expand_path('../support', __FILE__)
 
 module Skylab::Slake
   describe Task do
-    it "should build" do
-      task = Task.new(FakeParent)
+    it "should build and do parent stuff" do
+      task = Task.new
+      task.has_parent?.should eq(nil)
+      task.respond_to?(:parent_graph).should eq(false)
+      task.parent_graph = FakeParent
+      task.has_parent?.should eq(true)
       task.parent_graph.should eq(FakeParent)
     end
   end
