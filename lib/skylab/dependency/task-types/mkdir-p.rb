7372108f3e1ac4c458de120386b56ce65959f4a8
Mark Meves <mark.meves@gmail.com>
2012-01-22 23:00:24 -0800

    [#029] (whitespace only! de-indent)

--- lib/skylab/dependency/task-types/mkdir-p.rb
+++ lib/skylab/dependency/task-types/mkdir-p.rb
@@ -1,61 +1,58 @@
 require File.expand_path('../../task', __FILE__)
 require 'skylab/face/path-tools'
-require 'pathname'
 require 'fileutils'
 
-module Skylab
-  module Dependency
-    class TaskTypes::MkdirP < Task
+module Skylab::Dependency
+  class TaskTypes::MkdirP < Task
 
-      class Maker
-        # we need a dedicated class for this because the host object has its own mkdir_p
-        # which is different
-        include FileUtils
-        def initialize stream, prefix
-          @fileutils_output = stream
-          @fileutils_label = prefix
-        end
-        public :mkdir_p
-      end
-
-      include ::Skylab::Face::PathTools
+    class Maker
+      # we need a dedicated class for this because the host object has its own mkdir_p
+      # which is different
       include FileUtils
-      attribute :mkdir_p
-      attribute :maxdepth
+      def initialize stream, prefix
+        @fileutils_output = stream
+        @fileutils_label = prefix
+      end
+      public :mkdir_p
+    end
 
-      def check
-        @ok = true
-        if File.exist?(@mkdir_p)
-          true
+    include ::Skylab::Face::PathTools
+    include FileUtils
+    attribute :mkdir_p
+    attribute :maxdepth
+
+    def check
+      @ok = true
+      if File.exist?(@mkdir_p)
+        true
+      else
+        dir = @mkdir_p
+        current_depth = 0
+        begin
+          dir = File.dirname(dir)
+          current_depth += 1
+        end while ! File.directory?(dir) and ! ['.','/'].include?(dir) and current_depth <= @maxdepth
+        if current_depth > @maxdepth
+          _info("won't mkdir more than #{@maxdepth} levels deep " <<
+            "(#{pretty_path @mkdir_p} requires #{current_depth} levels)")
+          @ok = false
+          false
         else
-          dir = @mkdir_p
-          current_depth = 0
-          begin
-            dir = File.dirname(dir)
-            current_depth += 1
-          end while ! File.directory?(dir) and ! ['.','/'].include?(dir) and current_depth <= @maxdepth
-          if current_depth > @maxdepth
-            _info("won't mkdir more than #{@maxdepth} levels deep " <<
-              "(#{pretty_path @mkdir_p} requires #{current_depth} levels)")
-            @ok = false
-            false
-          else
-            _info "doesn't exist, can create #{pretty_path @mkdir_p}"
-            false
-          end
+          _info "doesn't exist, can create #{pretty_path @mkdir_p}"
+          false
         end
       end
+    end
 
-      def slake
-        check and return true # silent ok
-        ! @ok and return false
-        Maker.new(ui.err, "#{me}: ").mkdir_p(@mkdir_p, :verbose => true, :noop => request[:dry_run]) # _show_bash @todo
-        true
-      end
+    def slake
+      check and return true # silent ok
+      ! @ok and return false
+      Maker.new(ui.err, "#{me}: ").mkdir_p(@mkdir_p, :verbose => true, :noop => request[:dry_run]) # _show_bash @todo
+      true
+    end
 
-      def _defaults!
-        @maxdepth ||= 1
-      end
+    def _defaults!
+      @maxdepth ||= 1
     end
   end
 end
