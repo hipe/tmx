9c846146cffc702fd1355e72436bde5bbef0da1c
Mark Meves <mark.meves@gmail.com>
2012-01-23 06:00:08 -0800

    [#029] let's begin this MASSIVE REFACTOR

---
+++ lib/skylab/dependency/test/task-types/mkdir-p_spec.rb
@@ -0,0 +1,62 @@
+require File.expand_path('../support', __FILE__)
+require 'skylab/dependency/task-types/mkdir-p'
+
+module Skylab::Dependency::TestNamespace
+  include ::Skylab::Dependency
+  describe TaskTypes::MkdirP do
+    subject { TaskTypes::MkdirP }
+    it "builds an empty object" do
+      subject.new.should be_kind_of(TaskTypes::MkdirP)
+    end
+    context "as empty" do
+      subject { TaskTypes::MkdirP.new }
+      it "whines about required arg missing if you try to run it" do
+        lambda { subject.invoke }.should(
+          raise_exception(RuntimeError, /missing required attributes?: .*mkdir_p/)
+        )
+      end
+    end
+    context "when the required parameters are present" do
+      let(:dir_arg) { "#{BUILD_DIR}/foo/bar" }
+      subject do
+        TaskTypes::MkdirP.new( :mkdir_p => dir_arg )
+      end
+      context "with regards to dry_run" do
+        before { subject.context = context }
+        context "by default" do
+          let (:context) { { } }
+          it { should_not be_dry_run }
+        end
+        context "with dry run in context" do
+          let (:context) { { :dry_run => true } }
+          it { should be_dry_run }
+          context "when invoked" do
+            let (:stderr) { "" }
+            before do
+              BUILD_DIR.prepare
+              subject.event_listeners[:all] = lambda { |e| stderr << e.to_s }
+            end
+            context "a two-element do-hah" do
+              context "with default max_depth" do
+                it "will not go because it is past max depth" do
+                  subject.invoke
+                  stderr.should match(/more than 1 levels? deep/)
+                end
+              end
+              context "with max_depth increased to two" do
+                subject do
+                  TaskTypes::MkdirP.new(:mkdir_p => dir_arg, :max_depth => 2)
+                end
+                it "will go becuase it is equal to max depth" do
+                  subject.invoke
+                  stderr.should match(%r{mkdir -p .*foo/bar})
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
