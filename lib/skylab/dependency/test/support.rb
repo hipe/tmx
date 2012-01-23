04def870b1f77f5a99379b1b5466bf7d60256ad3
Mark Meves <mark.meves@gmail.com>
2012-01-22 23:35:35 -0800

    [#029] here is a sneak peak of some test support that you will use in the future

--- lib/skylab/dependency/test/support.rb
+++ lib/skylab/dependency/test/support.rb
@@ -1,3 +1,79 @@
-require File.expand_path('../../test-support/static-file-server', __FILE__)
-require File.expand_path('../../test-support/ui-tee', __FILE__)
+require File.expand_path('../../../../skylab', __FILE__)
+
+module Skylab
+  module Dependency
+  end
+end
+
+module Skylab::Dependency::TestNamespace
+
+  TEMP_DIR = ::Skylab::ROOT.join('tmp')
+
+  BUILD_DIR = Class.new(Pathname).class_eval do
+    include FileUtils
+    def fu_output_message msg
+      $stderr.puts "BUILD_DIR #{msg}"
+    end
+    def prepare
+      if directory?
+        fu_output_message("removing self #{self}")
+        remove_entry_secure to_s
+        mkdir to_s, :verbose => true
+      elsif dirname.directory?
+        mkdir to_s, :verbose => true
+      else
+        fail("BUILD_DIR won't make more than one directory. " <<
+             "Parent directory must first exist: #{dirname}")
+      end
+      true
+    end
+    self
+  end.new(TEMP_DIR.join('build-dependency'))
+
+  FILE_SERVER = Class.new.class_eval do
+    def run
+      @server ||= begin
+        require 'skylab/dependency/static-file-server'
+        ::Skylab::Dependency::StaticFileServer.new(FIXTURES_DIR)
+      end
+      @server.start_unless_running
+    end
+    self
+  end.new
+
+  FIXTURES_DIR = Pathname.new(File.expand_path('../fixtures', __FILE__))
+
+  module CustomExpectationMatchers
+    def be_including foo
+      BeIncluding.new(foo)
+    end
+    def be_subclass_of foo
+      BeSubclassOf.new(foo)
+    end
+  end
+
+  class BeIncluding
+    def initialize expected
+      @expected = expected
+    end
+    def matches? target
+      (@target = target).include? @expected
+    end
+    def failure_message
+      "expected #{@target.inspect} to include #{@expected}"
+    end
+    def negative_failure_message
+      "expected #{@target.inspect} not to include #{@expected}"
+    end
+  end
+
+ class BeSubclassOf < BeIncluding
+   def description
+     "be subclass of #{@expected}"
+   end
+    def matches? foo
+      super(foo.ancestors)
+    end
+  end
+end
 
