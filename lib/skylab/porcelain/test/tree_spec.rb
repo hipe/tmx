5df68f540632401bf0173497d19a253a9d6ca00f
Mark Meves <mark.meves@gmail.com>
2012-02-26 11:18:16 -0800

    [CLOSES #074] porcelain: tree: copy paste ..
      * copy-paste (and modify) from #026 tree related things

---
+++ lib/skylab/porcelain/test/tree_spec.rb
@@ -0,0 +1,76 @@
+# encoding: utf-8
+
+require File.expand_path('../../tree', __FILE__)
+
+module Skylab::Porcelain::TestNamespace
+  module ArrayExtension
+    class << self
+      def extend_to el
+        el.extend self
+        el.children and el.children.each do |_el|
+          extend_to _el
+        end
+        el
+      end
+      alias_method :[], :extend_to
+    end
+    def name     ; self[:name] end
+    def children ; self[:children] end
+    def children?; !! self[:children] end
+  end
+end
+
+module Skylab::Porcelain::TestNamespace
+  include Skylab::Porcelain
+  describe Tree do
+    it "renders a pretty tree" do
+      foo = ArrayExtension[
+        { :name => "document",
+          :children => [
+            { :name => "head" },
+            { :name => "body",
+              :children => [
+                { :name => "element1",
+                  :children => [
+                    { :name => "lone wolf",
+                      :children => [
+                        { :name => 'and cub' }
+                      ]
+                    }
+                  ]
+                },
+                { :name => "element2",
+                  :children => [
+                    { :name => "sub1" },
+                    { :name => "sub2" },
+                    { :name => "sub3" }
+                  ]
+                },
+                { :name => "element3", :children => [ { :name => "sub4" } ] }
+              ]
+            },
+            { :name => "foot" }
+          ]
+        }
+      ]
+      output = subject.view_tree foo
+      DERP = <<-HERE.gsub(/^        /, '')
+        document
+         ├head
+         ├body
+         │ ├element1
+         │ │ └lone wolf
+         │ │   └and cub
+         │ ├element2
+         │ │ ├sub1
+         │ │ ├sub2
+         │ │ └sub3
+         │ └element3
+         │   └sub4
+         └foot
+      HERE
+      output.should == DERP # use this form with --diff option
+    end
+  end
+end
+
---
+++ lib/skylab/porcelain/tree.rb
@@ -0,0 +1,104 @@
+# encoding: utf-8
+
+module Skylab
+  module Porcelain
+    # forward declaration for consistent indentation below
+  end
+end
+
+module Skylab::Porcelain::Tree
+  class Locus
+    def initialize opts={}
+      @branch      = ' ├'
+      @pipe        = ' │'
+      @last_branch = ' └'
+      @blank       = '  '
+      @name_accessor = :name
+      opts.each { |k, v| send("#{k}=", v) }
+    end
+    attr_accessor :branch, :pipe, :last_branch, :empty
+    attr_accessor :name_accessor
+    def traverse(root, &block)
+      @level = 0
+      @block = block
+      @prefix_stack = []
+      _traverse root
+    end
+    def _traverse root, meta={}
+      @block.call root, meta
+      sum = 1
+      if root.children?
+        _push meta
+        last = root.children.length - 1
+        root.children.each_with_index do |child, idx|
+          sum += _traverse(child, {
+            :is_first => (0 == idx),
+            :is_last  => (last == idx),
+            :level    => @level
+          })
+        end
+        _pop meta
+      end
+      sum
+    end
+    def _push meta
+      @level += 1
+      @prefix_stack.push parent_prefix(meta)
+    end
+    def _pop meta
+      @level -= 1
+      @prefix_stack.pop
+    end
+    def prefix meta
+      meta[:level].nil? and return ''
+      mine = case true
+             when (meta[:is_last]) ; @last_branch
+             else                  ; @branch
+             end
+      leader = @prefix_stack.join('')
+      "#{leader}#{mine}"
+    end
+    def parent_prefix meta
+      meta[:level].nil? and return ''
+      if meta[:is_first]
+        if meta[:is_last]
+          @blank
+        else
+          @pipe
+        end
+      elsif meta[:is_last]
+        @blank
+      else
+        @pipe
+      end
+    end
+  end
+end
+
+module Skylab::Porcelain
+  class << Tree
+    def from_paths paths
+      require File.expand_path('../tree/node', __FILE__)
+      Tree::Node.from_paths paths
+    end
+    def view_tree root, opts={}, &block
+      unless out = opts.delete(:out)
+        require 'stringio'
+        out = StringIO.new
+        return_string = true
+      end
+      loc = Tree::Locus.new opts
+      block ||= lambda do |node, meta|
+        out.puts "#{loc.prefix(meta)}#{node.send(loc.name_accessor)}"
+      end
+      sum = loc.traverse(root, &block)
+      if return_string
+        out.rewind
+        out.read
+      else
+        sum
+      end
+    end
+  end
+end
+
