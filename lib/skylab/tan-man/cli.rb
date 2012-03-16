2e24e54232919f6792f0769a7822a96488a7d7bd
Mark Meves <mark.meves@gmail.com>
2012-03-16 01:15:27 -0700

    [#077] tanman: remote: add (with awesome overhaul)

--- lib/skylab/tan-man/cli.rb
+++ lib/skylab/tan-man/cli.rb
@@ -1,153 +1,164 @@
 require File.expand_path('../..', __FILE__)
-require 'skylab/code-molester/config/file'
 require 'skylab/face/path-tools'
+require 'skylab/porcelain/attribute-definer'
 require 'skylab/porcelain/bleeding'
-require 'skylab/slake/attribute-definer'
-require 'skylab/test-support/test-support' # ick just for unindent
+require 'skylab/pub-sub/emitter'
 
-module Skylab::Asib
+module Skylab::TanMan
   Bleeding = Skylab::Porcelain::Bleeding
+  Porcelain = Skylab::Porcelain
+  PubSub = Skylab::PubSub
+
+  CONF_PATH = ->() { "#{ENV['HOME']}/.tanrc" }
+  MY_GRAPH = { :info => :all }
+  ROOT = Skylab::Face::MyPathname.new(File.expand_path('..', __FILE__))
 
   class Cli < Bleeding::Runtime
+    extend PubSub::Emitter
+    emits Bleeding::EVENT_GRAPH.merge(MY_GRAPH)
+    def initialize
+      super
+      @stdout = $stdout
+      on_all { |e| @stdout.puts e.payload.first }
+    end
+    attr_reader :stdout
   end
 
   module Actions
   end
 
-  class MyPathname < Pathname
-    def pretty
-      Skylab::Face::PathTools.pretty_path to_s
+  module Models
+  end
+
+  module MetaAttributes
+  end
+  class << MetaAttributes
+    def all
+      constants.map { |k| const_get(k) }
+    end
+    def [](*a)
+      a.map do |k|
+        konst = k.to_s.gsub(/(?:^|([a-z])_)([a-z])/) { "#{$1}#{$2.upcase}" }.intern
+        const_get konst
+      end
     end
   end
 
-  CONF_PATH = ->() { "#{ENV['HOME']}/.asibrc" }
-
-  class Config < Skylab::CodeMolester::Config::File
-    extend Skylab::Slake::AttributeDefiner
-
-
-
+  module MetaAttributes::Boolean extend Porcelain::AttributeDefiner
+    meta_attribute :boolean do |name, meta|
+      alias_method "#{name}?", name
+    end
   end
 
-  module ConfigMethods
-    attr_reader :config
-    def config?
-      @config ||= build_config { |o| o.on_all { |s| emit(:stderr, s) } }
-      !! @config
-    end
-    OnLoadConfig = Skylab::PubSub::Emitter.new(:all, :error => :all)
-    def build_config
-      yield(on = OnLoadConfig.new)
-      conf = Skylab::CodeMolester::Config::File.new(:path => CONF_PATH.call)
-      unless conf.exist?
-        on.emit(:error, "Config file not found. Expecting #{conf.pretty}. Try #{pre "#{runtime.program_name} config generate"}")
-        return false
+  module MetaAttributes::Default extend Porcelain::AttributeDefiner
+    meta_attribute :default
+  end
+  module MetaAttributes::Default::InstanceMethods
+    def set_defaults!
+      self.class.attributes.select { |k, v| v.key?(:default) }.each do |k, v|
+        send("#{k}=", v[:default].respond_to?(:call) ? v[:default].call : v[:default])
       end
-      if ! conf.valid?
-        on.emit(:error, "issue with #{conf.pretty}: #{conf.invalid_reason}")
-        return false
-      end
-      conf
     end
-    def config_init
-      @config = nil
+  end
+
+  module MetaAttributes::Pathname extend Porcelain::AttributeDefiner
+    meta_attribute :pathname do |name, _|
+      alias_method(after = "#{name}_after_pathname=", "#{name}=")
+      define_method("#{name}=") do |path|
+        send(after, path ? MyPathname.new(path.to_s) : path)
+        path
+      end
+    end
+  end
+
+  module MetaAttributes::Regex extend Porcelain::AttributeDefiner
+    meta_attribute :on_regex_fail
+    meta_attribute :regex do |name, meta|
+      alias_method(after = "#{name}_after_regex=", "#{name}=")
+      define_method("#{name}=") do |str|
+        if (re = meta[:regex]) =~ str
+          send(after, str)
+        else
+          error(meta[:on_regex_fail] || "#{str.inspect} did not match pattern for #{name}: /#{re.source}/")
+          str
+        end
+      end
+    end
+  end
+
+  module MetaAttributes::Required extend Porcelain::AttributeDefiner
+    meta_attribute :required
+  end
+  module MetaAttributes::Required::InstanceMethods
+    include Bleeding::Styles
+    def required_ok?
+      if (a = self.class.attributes.map.select { |k, h| h[:required] && send(k).nil? }).size.nonzero?
+        error( "missing required attribute#{'s' if a.size != 1}: " <<
+          "#{oxford_comma(a.map { |o| "#{pre o.first}" }, ' and ')}")
+      else
+        true
+      end
     end
   end
 
   class MyAction
     extend Bleeding::Action
-    extend ::Skylab::Slake::AttributeDefiner
-    include ConfigMethods
+    extend Bleeding::DelegatesTo
 
-    def self.inherited cls
-      cls.action_module_init
+    extend Porcelain::AttributeDefiner
+    meta_attribute MetaAttributes::Regex
+    meta_attribute MetaAttributes::Required
+    include MetaAttributes::Required::InstanceMethods
+
+    extend PubSub::Emitter
+    emits Bleeding::EVENT_GRAPH.merge(MY_GRAPH)
+
+    # might get promoted to runtime
+    def config
+      @config and return @config
+      require ROOT.join('models/config').to_s
+      @config = Models::Config.new(self, CONF_PATH).init
     end
 
-    meta_attribute :boolean
-    def self.on_boolean_attribute name, _
-      alias_method "#{name}?", name
-    end
-
-    meta_attribute :default
-
-    meta_attribute :pathname
-    def self.on_pathname_attribute name, _
-      alias_method "#{name}_before_pathname=", "#{name}="
-      define_method("#{name}=") do |path|
-        send("#{name}_before_pathname=", MyPathname.new(path.to_s))
-      end
-    end
-
-    def initialize *a
-      action_init(*a)
-      config_init
-      self.class.attributes.select { |k, v| v.key?(:default) }.each do |k, v|
-        send("#{k}=", v[:default].respond_to?(:call) ? v[:default].call : v[:default])
-      end
-    end
-
-    def skip m
-      emit(:info, "#{m}, skipping")
-      nil
-    end
-
-    def update_attributes! req
-      req.each { |k, v| send("#{k}=", v) }
-    end
-  end
-
-
-  #### "actions"
-  #
-  module Actions; end
-
-  class Actions::Put < MyAction
-    desc "put files to remote server (see config-make)"
-    option_syntax do |h|
-      on('-n', '--dry-run', "Dry run.") { h[:dry_run] = true }
-    end
-    def execute path
-      config? or return false
-
-
-      emit :info, "ok, sure: #{config.path}"
-    end
-  end
-
-  module Actions::Config
-    extend Bleeding::Namespace
-    desc "manage the config file"
-    summary { ["config file stuff (child actions: #{action_syntax})"] }
-  end
-
-  class Actions::Config::Generate < MyAction
-
-    desc "write the config file"
-
-    attribute :dest, :pathname => true, :default => CONF_PATH
-    attribute :dry_run, :boolean => true, :default => false
-
-    option_syntax do |h|
-      on('-n', '--dry-run', "dry run.") { h[:dry_run] = true }
-    end
-
-    def execute opts
-      update_attributes! opts
-      content = <<-HERE.unindent
-        host = yourhost
-        document_root = /path/to/your/doc/root
-      HERE
-      if dest.exist?
-        if content == dest.read
-          return skip("no change: #{dest.pretty}")
+    def format_error event
+      event.tap do |e|
+        if runtime.runtime
+          subj, verb, obj = [runtime.runtime.program_name, action.name, runtime.actions_module.name]
         else
-          return skip("exists, won't overwrite: #{dest.pretty}")
+          subj, verb = [runtime.program_name, action.name]
         end
+        e.payload[0] = "#{subj} failed to #{verb}#{" #{obj}" if obj}: #{e.message}"
       end
-      bytes = 0
-      dest.open('w+') { |fh| bytes = fh.write(content) } unless dry_run?
-      emit :info, "wrote #{dest.pretty} (#{bytes} bytes)"
-      true
+    end
+
+    def initialize runtime
+      super
+      @config = nil
+      @invalid_reasons = []
+      on_error { |e| @invalid_reasons.push(format_error(e)) }
+      on_all   { |e| runtime.emit(e.type, *e.payload) }
+    end
+
+    delegates_to :runtime, :stdout
+
+    def valid?
+      @invalid_reasons.size.nonzero? and return false
+      required_ok?
+      @invalid_reasons.size.zero?
+    end
+  end
+
+  module Actions::Remote
+    extend Bleeding::Namespace
+    desc "manage remotes."
+    summary { ["#{action_syntax} remotes"] }
+  end
+
+  class Actions::Remote::Add < MyAction
+    desc "add the remote."
+    def execute name, host
+      config or return help
+      config.add_remote(name, host) or help(invite_only: true)
     end
   end
 end
