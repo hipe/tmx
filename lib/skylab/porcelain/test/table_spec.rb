ef8fe142692463a471c6f15b75f667d1de4a214e
Mark Meves <mark.meves@gmail.com>
2012-03-17 03:57:08 -0700

    [#090] porcelain: table: column type inference: snapshot before changing logic

--- lib/skylab/porcelain/test/table_spec.rb
+++ lib/skylab/porcelain/test/table_spec.rb
@@ -20,18 +20,18 @@ module Skylab::Porcelain::TestSupport
       it "you can also just get a string back" do
         Porcelain.table([]).should eql("(empty)\n")
       end
-      it "renders a 2 x 2 table" do
-        data = [ %w(eenie meenie),
-                 %w(minie moe) ]
+      it "renders a 2 x 2 table (align left)" do
+        data = [ %w(derp mcderperson),
+                 %w(herper mcderpertino) ]
         Porcelain.table(data).should eql(<<-HERE.unindent)
-          eenie meenie
-          minie    moe
+          derp   mcderperson 
+          herper mcderpertino
         HERE
       end
       it "takes options the canonical way" do
         s = Porcelain.table([['a','b'],['c','dd']], :head => '<<', :tail => '>>', :separator => ' | ')
         s.should eql(<<-HERE.unindent)
-          <<a |  b>>
+          <<a | b >>
           <<c | dd>>
         HERE
       end
@@ -41,9 +41,47 @@ module Skylab::Porcelain::TestSupport
           o.on_all { |e| _stdout.puts e }
         end
         stdout.should eql(<<-HERE.unindent)
-          <<a |  b>>
+          <<a | b >>
           <<c | dd>>
         HERE
+      end
+    end
+    context 'infers types of columns and renders accordingly' do
+      let(:rendered_table) { Porcelain.table(row_enum) }
+      context 'with some everyday ordinary strings' do
+        let(:row_enum) do
+          [['meep', 'mop'],
+           ['pee',  'poo']]
+        end
+        it 'will align left' do
+          rendered_table.should eql(<<-HERE.unindent)
+            meep mop
+            pee  poo
+          HERE
+        end
+      end
+      context 'with columns whose every value is an integer-like string' do
+        let(:row_enum) do
+          [['123', '4567'],
+           ['6789', '-0']]
+        end
+        it 'will align right' do
+          rendered_table.should eql(<<-HERE.gsub(/^(?:  ){6}/, ''))
+             123 4567
+            6789   -0
+          HERE
+        end
+      end
+      context 'when you have floating-point like doo-hahs, something magic happens' do
+        let(:row_enum) do
+          [['1', '2'],
+           ['34','56'],
+           ['1.348', '-3.14'],
+           ['1234.5678', '0']]
+        end
+        it 'makes the decimal points line up', {wip:true} do
+          puts "OK:\n#{rendered_table}"
+        end
       end
     end
   end
