f65365e2a9e5f1f65f61564d17fff44751110039
Mark Meves <mark.meves@gmail.com>
2012-04-04 18:48:03 -0700

    [#094.1] code-molester various: clean refactor with features
      * add (uncovered) modified? check
      * internally, state is managed with a @state variable as a state machine
          rather than a @valid variable (state machine pattern)
      * experimentally DRY up implementation with some custom (class-specific) DSL
        * wtf!
      * change implementation so that we don't confusingly have two isomporphic variables
        * instead we confusingly have one for two different types of data (isomorphic though!!!!!)

--- lib/skylab/code-molester/test/config/file_spec.rb
+++ lib/skylab/code-molester/test/config/file_spec.rb
@@ -22,7 +22,7 @@ describe ::Skylab::CodeMolester::Config::File do
     config.valid?.should eql(true)
   end
   def unparses_ok
-    t = config.content_tree
+    t = config.sexp
     unp = t.unparse
     par = content
     unp.should eql(par)
@@ -41,7 +41,7 @@ describe ::Skylab::CodeMolester::Config::File do
       it "has no content items" do
         subject = self.subject
         subject.content_items.size.should eql(0)
-        subject.content.should eql('')
+        subject.string.should eql('')
       end
     end
     context "when input is bunch of blank lines" do
@@ -50,7 +50,7 @@ describe ::Skylab::CodeMolester::Config::File do
         subject = self.subject
         subject.invalid_reason.should eql(nil)
         subject.content_items.size.should eql(0)
-        subject.unparse.should eql(input_string)
+        subject.string.should eql(input_string)
       end
     end
     context "when input is one comment" do
@@ -58,7 +58,7 @@ describe ::Skylab::CodeMolester::Config::File do
       it "it is valid" do
         subject.invalid_reason.should eql(nil)
         subject.content_items.size.should eql(0)
-        subject.content.should eql("      # ha-blah")
+        subject.string.should eql("      # ha-blah")
       end
     end
     context "when input is one assigmnent line" do
@@ -221,11 +221,11 @@ describe ::Skylab::CodeMolester::Config::File do
       it "you can set existing values" do
         config.value_items['foo'] = 'blamo'
         config.value_items['foo'].should eql('blamo')
-        config.content.split("\n").first.should eql("foo = blamo")
+        config.string.split("\n").first.should eql("foo = blamo")
       end
       it "you can create new values" do
         config['bleuth'] = 'michael'
-        config.content.should eql(<<-HERE.unindent.strip)
+        config.string.should eql(<<-HERE.unindent.strip)
           foo = bar
           biff = baz
           bleuth = michael
@@ -261,11 +261,11 @@ describe ::Skylab::CodeMolester::Config::File do
       let(:config) { klass.new(:path => path) }
       def is_valid
         config.valid?.should eql(true)
-        config.content_tree.should be_kind_of(Array)
+        config.sexp.should be_kind_of(Array)
       end
-      it "It knows it doesn't exist, and reports having the empty string as content" do
+      it "It knows it doesn't exist, and the string() of it will be the empty string" do
         config.exist?.should eql(false)
-        config.content.should eql('')
+        config.string.should eql('')
       end
       it "It sees itself as valid, and will even show you a parse tree" do
         is_valid
@@ -296,7 +296,7 @@ describe ::Skylab::CodeMolester::Config::File do
         context "lets you add new values" do
           it "to the root node (note the inherited whitespace)" do
             config['new_item'] = 'new value'
-            config.content.split("\n")[0,3].join("\n").should eql(<<-HERE.unindent.strip)
+            config.string.split("\n")[0,3].join("\n").should eql(<<-HERE.unindent.strip)
               who = hah
                 boo = bah
                 new_item = new value
@@ -312,10 +312,10 @@ describe ::Skylab::CodeMolester::Config::File do
           end
           it "lets you create a section by assigning a hash to it" do
             last_part = ->(s) { s.match(/good times here(.+)\z/m)[1] }
-            last_part[config.content].should eql("\n")
+            last_part[config.string].should eql("\n")
             config['goal'] ||= {}
             config['goal']['dream'] = 'deadline'
-            last_part[config.content].should eql(<<-HERE.gsub(/^(  ){6}/, ''))
+            last_part[config.string].should eql(<<-HERE.gsub(/^(  ){6}/, ''))
 
             [goal]
               dream = deadline
