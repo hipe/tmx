b9094a251319ed1b3b3eee612b8027634dacdb1d
Mark Meves <mark.meves@gmail.com>
2012-06-05 12:38:29 -0400

    [#049.910] tmx bleed path init works.

--- lib/skylab/tmx/model/config.rb
+++ lib/skylab/tmx/model/config.rb
@@ -2,20 +2,14 @@ require 'skylab/code-molester/config/file'
 
 
 module Skylab::Tmx
-  module Model; end
+  module Model
+  end
   class Model::Config < ::Skylab::CodeMolester::Config::File
-    PATH = '~/.tmx'
-    emits :all,
-          :error     => :all,
-          :info      => :all,
-          :info_head => :all,
-          :info_tail => :all,
-          :out       => :all
+    PATH = '~/.tmxconfig'
   end
   class << Model::Config
-    def build &b
-      path = Model::Config::PATH.sub(/^~/) { ENV["HOME"] }
-      new(path, &b)
+    def build
+      new( Model::Config::PATH.sub(/^~/) { ENV["HOME"] } )
     end
   end
 end
--- lib/skylab/tmx/modules/bleed/api.rb
+++ lib/skylab/tmx/modules/bleed/api.rb
@@ -1,35 +1,30 @@
 require 'pathname'
 
-module Skylab
-  module Tmx
-    module Bleed
-      API_ROOT = Pathname.new('../api').expand_path(__FILE__)
-      module Api
-        require API_ROOT.join('action')
-        module Actions
-        end
-      end
+module Skylab::Tmx::Modules::Bleed
+  API_ROOT = Pathname.new('../api').expand_path(__FILE__)
+  module Api
+    require API_ROOT.join('action').to_s
+    module Actions
     end
   end
-end
-
-module Skylab::Tmx::Bleed
   class << Api
     def build &b
-      Api::Dispatcher.new(&b)
+      b and raise ArgumentError.new("do not pass a block here for now")
+      Api::Dispatcher.new
     end
   end
   class Api::Dispatcher
     ACTIONS_ROOT = API_ROOT.join('actions')
     def initialize &events
-      @events = events or fail("must pass event handlers when building api.")
+      events and raise ArgumentError.new("do not pass block here for now")
     end
-    def invoke act, ctx
-      require ACTIONS_ROOT.join(act.join('/'))
+    def invoke act, ctx, &events
+      events or raise ArgumentError.new("expected block here")
+      require ACTIONS_ROOT.join(act.join('/')).to_s
       klass = act.reduce(Api::Actions) do |m, s|
         m.const_get(s.to_s.gsub(/(^[a-z]|[a-z](?=-))(?:-(.))?/) { "#{$1.upcase}#{$2}" })
       end
-      klass.new(ctx, &@events).invoke
+      klass.new(ctx, &events).invoke
     end
   end
 end
