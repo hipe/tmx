c8084ef4a35c2199ca599c15274c059243cd6a49
Mark Meves <mark.meves@gmail.com>
2012-06-05 15:56:06 -0400

    [CLOSES #049.920] bleed path: get and set path

--- lib/skylab/tmx/modules/bleed/api/action.rb
+++ lib/skylab/tmx/modules/bleed/api/action.rb
@@ -1,17 +1,39 @@
 require 'skylab/tmx/model/config'
 
 module Skylab::Tmx::Modules::Bleed::Api
+  module MyPathTools
+    def contract_tilde path
+      path.sub(%r{^#{Regexp.escape ENV['HOME']}/}, '~/')
+    end
+  end
   class Action
     extend ::Skylab::PubSub::Emitter
-    emits :all,  info: :all
+    emits :all,  info: :all, error: :all,
+      head: :all, tail: :all
+
+    include MyPathTools
+
     def config
       @config ||= ::Skylab::Tmx::Model::Config.build
     end
+    def config_write
+      config.write do |o|
+        o.on_before    { |e| emit(:head, "config: #{e.message}") ; e.touch! }
+        o.on_after     { |e| emit(:tail, " .. done (wrote #{e.bytes} bytes).") ; e.touch! }
+        o.on_no_change { |e| emit(:info, e.message) ; e.touch! }
+        o.on_all       { |e| emit(:info, "handle me-->#{e.type}<-->#{e.message}" ) unless e.touched? }
+      end
+    end
+    def error msg
+      emit :error, msg # child action class must actualy declare that it emits this
+      false
+    end
     def initialize ctx
       @config = nil
-      @ctx = ctx
+      @params = ctx
       yield self
     end
+    attr_reader :params
   end
 end
 
--- lib/skylab/tmx/modules/bleed/api/actions/init.rb
+++ lib/skylab/tmx/modules/bleed/api/actions/init.rb
@@ -1,19 +1,14 @@
 module Skylab::Tmx::Modules::Bleed::Api
   class Actions::Init < Action
-    emits head: :all, tail: :all
     def invoke
       if config.exist?
         emit :info, "exists, won't overwrite: #{config.pretty}"
         return nil
       end
-      root = ::Skylab::ROOT.to_s.sub(%r{^#{Regexp.escape ENV['HOME']}/}, '~/')
+      path = contract_tilde(::Skylab::ROOT.to_s)
       config['bleed'] ||= {} # create the section called [bleed]
-      config['bleed']['root'] = root
-      config.write do |o|
-        o.on_before   { |e| emit(:head, "config: #{e.message}") ; e.touch! }
-        o.on_after    { |e| emit(:tail, " .. done (wrote #{e.bytes} bytes).") ; e.touch! }
-        o.on_all      { |e| emit(:info, "handle me-->#{e.type}<-->#{e.message}" ) unless e.touched? }
-      end
+      config['bleed']['path'] = path
+      write_config
     end
   end
 end
