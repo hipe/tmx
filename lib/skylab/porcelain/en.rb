27da4ba3229d63728e2292feda5e8b2a343510a6
Mark Meves <mark.meves@gmail.com>
2012-06-16 14:07:51 -0700

    [#102.300.4.3.1] en.rb has dedicated test file

--- lib/skylab/porcelain/en.rb
+++ lib/skylab/porcelain/en.rb
@@ -15,31 +15,3 @@ module Skylab::Porcelain
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  require 'rspec/autorun'
-  describe "oxford_commma" do
-    include Skylab::Porcelain::En
-    let(:subject) { oxford_comma arr }
-    context(a = %w()) do
-      let(:arr) { a }
-      specify { should eql('') }
-    end
-    context(b = %w(eenie)) do
-      let(:arr) { b }
-      specify { should eql('eenie') }
-    end
-    context(c = %w(eenie meenie)) do
-      let(:arr) { c }
-      specify { should eql('eenie or meenie') }
-    end
-    context(d = %w(eenie meenie miney)) do
-      let(:arr) { d }
-      specify { should eql('eenie, meenie or miney') }
-    end
-    context(e = %w(eenie meenie miney moe)) do
-      let(:arr) { e }
-      specify { should eql('eenie, meenie, miney or moe') }
-    end
-  end
-end
-
