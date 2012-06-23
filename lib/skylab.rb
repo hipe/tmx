8c27caf04016be575e26faff3ec015481587959f
Mark Meves <mark.meves@gmail.com>
2012-06-22 19:58:52 -0700

    [#102.05.04.02] HAHAHAHAHAHA
      * hahahah
      * 48 LOC to 13 LOC wtf
      * what the hell is wrong with me

--- lib/skylab.rb
+++ lib/skylab.rb
@@ -7,14 +7,12 @@ module Skylab
 end
 
 
-require 'strscan'
-
 module Skylab::Autoloader
   # experimental.  const_missing hax can suck, so use this only if it's compelling.
   # a bit of a mess now until things settle down
   class << self
     def extended mod
-      dir = mod.dir_path = guess_dir(mod, %r{^(.+)\.rb:\d+:in `}.match(caller[0])[1])
+      dir = mod.dir_path = guess_dir(mod.to_s, %r{^(.+)\.rb:\d+:in `}.match(caller[0])[1]) { |e| fail(e) }
       mod.singleton_class.send(:alias_method, :orig_const_missing, :const_missing)
       mod.singleton_class.send(:define_method, :const_missing) do |const|
         path = "#{dir}/#{const.to_s.gsub(/(?<=^|([a-z]))([A-Z])/) { "#{'-' if $1}#{$2}" }.downcase}"
@@ -25,56 +23,22 @@ module Skylab::Autoloader
         const_get const
       end
     end
-    def guess_dir cons, path
-      no = catch(:parse_failure) do
-        p = BackwardsTokenizer.new(path.reverse, %r{[^/]+}, %r{/} )
-        c = BackwardsTokenizer.new(cons.reverse, /[^:]+/, /::/) { |s| s.gsub(/([A-Z](?=[a-z]))/){"#{$1}-"}.downcase }
-        if p.next == c.next
-          c.curr == p.peek and return p.rest                              # a/b/b a::b
-          return "#{p.rest}/#{c.current}"                                 # a/b   a::b
-        end
-        p.peek == c.curr and return p.rest                                # a/b/c a::b
-        p.curr == c.peek and return "#{p.rest}/#{p.current}/#{c.current}" # a/b   a::b::c
-        p.peek == c.peek and return "#{p.rest}/#{c.current}"              # a/b/p a::b::q
-        throw(:parse_failure, "failed to infer path for #{cons} from #{path}")
-        nil
+    PATH_RE = %r{\A(?:(?:(?<rest>|.*[^/])/+)?(?<peek>[^/]*)/+)?(?<curr>[^/]*)/*\z}
+    CONS_RE = %r{\A(?:::)?(?:(?:(?<rest>[^:]+(?:::[^:]+)*)::)?(?<peek>[^:]+)::)?(?<curr>[^:]+)\z}
+    def guess_dir const, path, &error
+      prest, ppeek, pcurr = PATH_RE.match(path ).values_at(1..3)
+      cpeek, ccurr = CONS_RE.match(const).values_at(2..3).map { |s| s.gsub(/(?<=[a-z])([A-Z])/){"-#{$1}"}.downcase if s }
+      parts = if ppeek == ccurr       ; [prest, ppeek]                  # a::b     a/b/c   # a/b/b a::b
+      elsif pcurr == ccurr            ; [prest, ppeek, pcurr]           # a::b     a/b     # a     a
+      elsif pcurr == cpeek            ; [prest, ppeek, pcurr, ccurr]    # a::b::c  a/b
+      elsif ppeek && ppeek == cpeek   ; [prest, ppeek, ccurr]           # a::b::d  a/b/f
       end
-      no and raise(no)
+      parts ? parts.compact.join('/') : error.call("failed to infer path for #{const} from #{path}")
     end
   end
   attr_accessor :dir_path
   def dir
     @dir ||= Pathname.new(dir_path)
-  end
-  class ThrowingStringScanner < StringScanner
-    %w(scan skip).each do |meth|
-      alias_method("soft_#{meth}", meth)
-      define_method(meth) do |pat|
-        x = super(pat) or
-          throw(:parse_failure, "failed to match #{pat.inspect} near #{(rest[0..10] << '(..)').inspect}")
-      end
-    end
-    alias_method :strscan_peek, :peek
-    def snoop re ; idx = match?(re) and strscan_peek(idx) end
-  end
-  class SimpleTokenizer < ThrowingStringScanner
-    attr_reader :curr
-    def initialize str, *a, &b
-      super(str)
-      @token_pattern, @separator_pattern = a
-      @filter = b || ->(s){s}
-    end
-    def next
-      @curr = @filter.call(scan @token_pattern)
-      soft_skip @separator_pattern
-      @curr
-    end
-    def peek ; s = snoop(@token_pattern) and @filter.call(s) end
-    alias_method :strscan_rest, :rest
-  end
-  class BackwardsTokenizer < SimpleTokenizer
-    def current ; @curr.reverse  end
-    def rest    ; super.reverse  end
   end
 end
 
--- lib/skylab/meta-hell/test/autoloader_spec.rb
+++ lib/skylab/meta-hell/test/autoloader_spec.rb
@@ -13,12 +13,16 @@ describe Skylab::Autoloader do
         it { should eql(dir) }
       end
     end
-    with "tan-man/models/config.rb", "TanMan::Models::Config", "tan-man/models/config", "(same folder)"
-    with "treemap/r/client.rb", "Treemap::R", "treemap/r", "(peek one level up)"
-    with "tm/cli.rb", "TM::CLI::Actions", "tm/cli/actions", "(infer one level down)"
-    with "sl/issue/api.rb", "SL::Issue::Models", "sl/issue/models", "(one up and one down)"
-    with 'sl/test-support/test-support.rb', 'SL::TestSupport', 'sl/test-support', "(double deuce)"
-    with 'foo', 'Foo', 'foo', '(shallow path)'
+    with "foo", "Foo", "foo", "(conventional atomic case)"
+    with "foo/bar", "Foo::Bar", 'foo/bar', "(conventional, plural)"
+    with "foo/bar", "Foo", "foo", "(peek one level up atomic case)"
+    with "C:\>///foo/foo", "Foo", "C:\>/foo", "(double deuce edge case)"
+    with "tan-man/models/config", "TanMan::Models::Config", "tan-man/models/config", "(longer conventional case)"
+    with "treemap/r/client", "Treemap::R", "treemap/r", "(peek one level up)"
+    with "appkin", "Appkin::Nodule", "appkin/nodule", "(infer one level down, atomic case)"
+    with "tm/cli", "TM::CLI::Actions", "tm/cli/actions", "(infer one level down, representative case)"
+    with "sl/issue/api", "SL::Issue::Models", "sl/issue/models", "(one up and one down)"
+    with 'sl/test-support/test-support', 'SL::TestSupport', 'sl/test-support', "(double deuce)"
   end
   context "may fail" do
     let(:subject) do
