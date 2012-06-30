be29d87745d68cf03ef4cf4b27286720363c4c49
Mark Meves <mark.meves@gmail.com>
2012-06-29 22:40:35 -0700

    [#102.05.04.04.02] add good coverage for bleeding level 2

--- lib/skylab/porcelain/test/bleeding/runtime_spec.rb
+++ lib/skylab/porcelain/test/bleeding/runtime_spec.rb
@@ -11,8 +11,17 @@ module ::Skylab::Porcelain::Bleeding::TestSupport
     end
   end
   describe "#{Bleeding::Runtime}" do
+    include ::Skylab::MetaHell::KlassCreator::InstanceMethods
+    attr_reader :cli_runtime
+    let(:_emit_spy) { ::Skylab::TestSupport::EmitSpy.new }
+    let(:emit_spy) do
+      @cli_runtime = my_klass.new(es = _emit_spy)
+      es.formatter = ->(e) { "#{e.type.inspect}<-->#{e.message.inspect}" }
+      @invoke_result = @cli_runtime.invoke(argv)
+      es
+    end
+    attr_reader :invoke_result, :my_klass, :subject
     context "at level 0" do
-      include ::Skylab::MetaHell::KlassCreator::InstanceMethods
       before(:all) do
         @my_klass = self.klass(:MyKloss, extends: Bleeding::Runtime) do # @todo try deeper names
           o = self
@@ -32,15 +41,6 @@ module ::Skylab::Porcelain::Bleeding::TestSupport
           end
         end
       end
-      attr_reader :cli_runtime
-      let(:_emit_spy) { ::Skylab::TestSupport::EmitSpy.new }
-      let(:emit_spy) do
-        @cli_runtime = my_klass.new(es = _emit_spy)
-        es.formatter = ->(e) { "#{e.type.inspect}<-->#{e.message.inspect}" }
-        @invoke_result = @cli_runtime.invoke(argv)
-        es
-      end
-      attr_reader :invoke_result, :my_klass, :subject
       _USAGE_RE =  /usage.+DORP <action> \[opts\] \[args\]/i
       _INVITE_RE =  /try DORP \[<action>\] -h for help/i
       should_usage_invite = ->(_) do
@@ -70,27 +70,134 @@ module ::Skylab::Porcelain::Bleeding::TestSupport
         specify { should be_event(3, /act2.+fooible/i) }
         specify { should be_event(4, /for help on a particular action/i) }
       end
-      context "with help request at LVL0 (as -h)" do
+      context "-h" do
         before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
         let(:argv) { ['-h'] }
         should_show_index
       end
-      context "with help request at LVL0 (as help)" do
+      context "help" do
         before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
         let(:argv) { ['help'] }
         should_show_index
       end
-      context "with a request for help on a valid action" do
+      context "-h <valid action>" do
         before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
         let(:argv) { ['-h', 'act2' ] }
         specify { should be_event(0, "usage: DORP act2 <fizzle> <bazzle>") }
         specify { should be_event(1, "description: fooibles your dooibles") }
       end
-      context "with a request for help on an invalid action" do
+      context "-h <invalid action>" do
         before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
         let(:argv) { ['-h', 'whatevr' ] }
         specify { should be_event(0, /invalid command.+whatevr.+expecting.+act1.+act2/i) }
         specify { should be_event(1, nil) }
+      end
+    end
+    context "at level 1 (the command 'pony')" do
+      before(:all) do
+        @my_klass = self.klass(:MyKliss, extends: Bleeding::Runtime) do
+          o = self
+          include RuntimeInstanceMethods
+          class o::MyAction
+            extend Bleeding::Action
+          end
+          module o::Actions
+          end
+          module o::Actions::Pony
+            extend Bleeding::Namespace
+          end
+          class o::Actions::Pony::Create < o::MyAction
+          end
+          class o::Actions::Pony::PutDown < o::MyAction
+            def execute oingo=nil, boingo
+              emit(:ze_payload, "yerp-->#{oingo.inspect}<-->#{boingo.inspect}<--")
+              :you_put_down_the_pony
+            end
+          end
+          class o::Actions::Pony::PutUp < o::MyAction
+            option_syntax.help_enabled = true
+            def execute
+              emit(:mein_payload, "yoip")
+            end
+          end
+        end
+      end
+      context "just it" do
+        before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+        let(:argv) { [ 'pony' ] }
+        it "shouldn't actually require an extended module as a namespace, just a plain module @todo"
+         specify { should be_event(0, /expecting.+create.+put-down/i) }
+      end
+      context "-h it" do
+        it "is a bug @todo"
+      end
+      context "it -h" do
+        it "is a bug @todo"
+      end
+      context "at level 2" do
+        _INVALID_EXPECTING = /invalid command.+nerk.+expecting.+create.+put-down/i
+        context "with a bad name" do
+          context "just it" do
+            before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+            let(:argv) { [ 'pony', 'nerk' ] }
+            specify { should be_event(0, _INVALID_EXPECTING) }
+            it "should be consistent in its use of the term {action|command} @todo"
+          end
+          context "-h it" do
+            before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+            let(:argv) { ['pony', '-h', 'nerk'] }
+            specify { should be_event(0, :error, _INVALID_EXPECTING) }
+          end
+          context "it -h" do
+            before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+            let(:argv) { ['pony', 'nerk', '-h'] }
+            specify { should be_event(0, :help, _INVALID_EXPECTING) }
+          end
+        end
+        context "with an amibiguous name" do
+          before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+          let(:argv) { ['pony', 'put'] }
+          specify { should be_event(0, :help, /ambiguous comand .+put.+did you mean put-down or put-up\?/i) }
+        end
+        context "with a good name" do
+          context "unambiguous fuzzy" do
+            _USAGE = /usage.+DORP pony put-down \[<oingo>\] <boingo>/i
+            context "just it does cute sytax thing" do
+              before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+              let(:argv) { [ 'pony', 'put-d' ] }
+              it "should use invoke() not execute() as the main doo-hah"
+              specify { should be_event(0, :syntax_error, /missing.+argument.+boingo/i) }
+              specify { should be_event(1, _USAGE) }
+              specify { should be_event(2, /try DORP pony put-down -h for help/i) }
+            end
+            context "-h it" do
+              before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+              let(:argv) { ['pony', '-h', 'put-d'] }
+              specify { should be_event(0, :help, _USAGE) }
+              specify { should be_event(1, nil) }
+            end
+            context "it -h out of the box will process it as an arg" do
+              before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+              let(:argv) { ['pony', 'put-d', '-h'] }
+              specify { should be_event(0, :ze_payload, 'yerp-->nil<-->"-h"<--') }
+              specify { should be_event(1, nil) }
+            end
+          end
+          context "exact match, with a thing with no option syntax but help enabled" do
+            _TRAILING_SPACE = "usage: DORP pony put-up "
+            context "it -h" do
+              before(:all) { _emit_spy.no_debug! ; @subject = emit_spy.stack }
+              let(:argv) { ['pony', 'put-up', '-h'] }
+              it "fix trailing space @todo"
+              specify { should be_event(:help, _TRAILING_SPACE) }
+            end
+            context "-h it" do
+              before(:all) { _emit_spy.debug! ; @subject = emit_spy.stack }
+              let(:argv) { ['pony', '-h', 'put-up'] }
+              specify { should be_event(:help, _TRAILING_SPACE) }
+            end
+          end
+        end
       end
     end
   end
