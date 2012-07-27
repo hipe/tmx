c2e2b018a1da3d354f5049f4fb26ba0ba404c82b
Mark Meves <mark.meves@gmail.com>
2012-07-27 06:41:13 -0400

    [#102.5.4.4.2.2.2.3.2.4.2.5-move] event loop matures
      * we realize that "cursor" and "mouse position" are not the same

--- src/information-tactics/hipe/hipe.d/demo.c
+++ src/information-tactics/hipe/hipe.d/demo.c
@@ -1,40 +1,73 @@
 #include "demo.h"
 
+#define HIPE_DEMO_COL_MAIN     5
+#define HIPE_DEMO_ROW_MICRO    1
+#define HIPE_DEMO_ROW_MINI     2
+#define HIPE_DEMO_ROW_FEED     3
+#define HIPE_DEMO_ROW_ANNOUCE  4
+
+#define HIPE_DEMO_DELAY_TENTHS 7
+
 typedef struct hipe_demo hipe_demo;
 
 struct hipe_demo {
   bool (*run)(hipe_demo *);
 
-  bool (*get_char)(hipe_demo *);
-  bool (*on_char)(hipe_demo *, int c);
-  bool (*on_mouse)(hipe_demo *);
+  bool (*char_entered)(hipe_demo *, int c);
+  bool (*mouse_buttoned)(hipe_demo *);
+  bool (*mouse_moved)(hipe_demo *, unsigned int y, unsigned int x);
 
   hipe_div *div;
 };
 
-bool hipe_demo_getch(hipe_demo* demo) {
-  int c;
-  bool keep_going;
-  switch (c = getch()) {
-    case     KEY_MOUSE : keep_going = demo->on_mouse(demo);   break;
-    default            : keep_going = demo->on_char(demo, c); break;
-  }
-  return keep_going;
+void hipe_demo_tick(unsigned int little, unsigned int big) {
+  mvprintw(HIPE_DEMO_ROW_MICRO, HIPE_DEMO_COL_MAIN, "event tick %5d/%7d", little, big); refresh();
 }
 
-bool hipe_demo_on_char(hipe_demo *demo, int c) {
-  if ('q' == c) return false;
-  mvprintw(3, 5, "some character: %c (do 'q' for quit)", c);
-  refresh();
+bool hipe_demo_event_loop(hipe_demo *demo) {
+  int prev_y = -1 ; int prev_x = -1 ; int y ; int x ;
+  halfdelay(HIPE_DEMO_DELAY_TENTHS); // overrides cbreak maybe?
+  bool do_loop = false;
+  int __tic = 0 ; int _ = 0 ;
+  do {
+    hipe_demo_tick(_, ++__tic);
+    getyx(demo->div->window, y, x);
+    if (prev_y != y || prev_x != x) {
+      prev_y = y ; prev_x = x ;
+      _++;
+      mvprintw(HIPE_DEMO_ROW_MINI, HIPE_DEMO_COL_MAIN, "cursor moved to y:%d x:%d (tick %d)", y, x, __tic); refresh();
+      do_loop = demo->mouse_moved(demo, y, x);
+    } else {
+      bool do_ch;
+      do {
+        do_ch = false;
+        int c = getch();
+        switch (c) {
+          case       ERR : do_loop = true ; break ; // nothing was pressed before halfdelay expired.
+          case KEY_MOUSE : _++; do_loop = do_ch = demo->mouse_buttoned(demo)   ; break ;
+          default        : _++; do_loop = do_ch = demo->char_entered(demo, c)  ; break ;
+        }
+      } while (do_ch);
+    }
+  } while(do_loop);
   return OKAY;
 }
 
-bool hipe_demo_on_mouse(hipe_demo *demo) {
+bool hipe_demo_char_entered(hipe_demo *demo, int c) {
+  mvprintw(HIPE_DEMO_ROW_FEED, HIPE_DEMO_COL_MAIN, "char entered: %c", c); refresh();
+  if ('q' == c) return false;
+  return true;
+}
+
+bool hipe_demo_mouse_buttoned(hipe_demo *demo) {
   MEVENT e;
   if (getmouse(&e)) return FAILED;
-  mvprintw(2, 5, hipe_mouse_event_describe(&e));
-  refresh();
-  return OKAY;
+  mvprintw(HIPE_DEMO_ROW_ANNOUCE, HIPE_DEMO_COL_MAIN, hipe_mouse_event_describe(&e)); refresh();
+  return true;
+}
+
+bool hipe_demo_mouse_moved(hipe_demo *demo, unsigned int y, unsigned int x) {
+  return true;
 }
 
 bool hipe_demo_run(hipe_demo *demo) {
@@ -43,20 +76,20 @@ bool hipe_demo_run(hipe_demo *demo) {
   cbreak();
   keypad(stdscr, TRUE);
   hipe_div *d = hipe_div_create(20, 60, 5, 5);
-  d->draw(d);
   demo->div = d;
-  while (demo->get_char(demo));
+  d->draw(d);
+  bool result = hipe_demo_event_loop(demo);
   free(d);
   demo->div = NULL;
-  return OKAY;
+  return result;
 }
 
 hipe_demo *hipe_demo_create() {
   hipe_demo *d = malloc(sizeof(hipe_demo));
   d->run = hipe_demo_run;
-  d->get_char = hipe_demo_getch;
-  d->on_char = hipe_demo_on_char;
-  d->on_mouse = hipe_demo_on_mouse;
+  d->char_entered   = hipe_demo_char_entered;
+  d->mouse_buttoned = hipe_demo_mouse_buttoned;
+  d->mouse_moved    = hipe_demo_mouse_moved;
   return d;
 }
 
