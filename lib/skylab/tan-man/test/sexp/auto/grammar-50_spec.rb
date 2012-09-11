73e18b2792b2bdb7028aa99e27c045765670d128
Mark Meves <mark.meves@gmail.com>
2012-09-11 12:05:11 -0400

    102.5.4.4.2.2.2.3.2.4.2.5.1.3.5.2.3
      - we have to fenangle the grammar to accomodate semantic whitespace :/
        - the purity of the generated grammar appears lost

--- lib/skylab/tan-man/test/sexp/auto/grammar-50_spec.rb
+++ lib/skylab/tan-man/test/sexp/auto/grammar-50_spec.rb
@@ -7,5 +7,8 @@ describe "#{::Skylab::TanMan::Sexp::Auto} list pattern (grammar 50)" do
     using_input '100-one-arc.dot' do
       it_should_unparse_losslessly
     end
+    using_input '110-two-arcs.dot' do
+      it_should_unparse_losslessly
+    end
   end
 end
