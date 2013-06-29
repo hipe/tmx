83bc21883f94385adbf963c4c394e45307767c9c
Mark Meves <mark.meves@gmail.com>
2013-06-29 05:15:25 -0400

    [mh] (#multi 2 of 2) add FUN.parse_with_ordered_set ((198))
     • add ability to specify irregular output file in bash script

--- lib/skylab/meta-hell/fun.rb
+++ lib/skylab/meta-hell/fun.rb
@@ -50,7 +50,7 @@ module Skylab::MetaHell
   # the position of the actual arguments still is not freeform - they must
   # occur in the same order with respect to each other as they occur in the
   # formal arguments. such a grammar would be possible but is beyond this
-  # scope (tracked by [#mh-027]).
+  # scope (see `parse_with_ordered_set`)
 
   o[:parse_series] = -> args, *f_a do
     o[:_parse_series][ args, f_a, -> e do
@@ -83,6 +83,68 @@ module Skylab::MetaHell
       ai += 1
     end
     res
+  end
+
+  # `parse_with_ordered_set` result is array of same length as `set_a`.
+  # consider using `curry` on it to create a parser object.
+  # sorta like the packrat parser algorithm, `argv` (if any) will be evaluated
+  # against each remaining element in `set_a`. each element of `set_a` is
+  # assumed to be a function that takes one arg: `argv`. if that function's
+  # result is anything other than nil, it is taken to mean it succeeded in
+  # matching (and should probably have shifted one or more elements off of
+  # `argv`). if the function matched, the function is removed from the
+  # running. the parse is finished and the function yields a result when
+  # either `argv` is empty or there are no more unmatched elements in `set_a`
+  # *or* a matching function could not be found for the current front element
+  # of `argv`.  (this function is [#027].)
+  # like so:
+  #
+  #     PARSER = MetaHell::FUN.parse_with_ordered_set.curry[ [
+  #       -> args { args.shift if args.first =~ /bill/i },
+  #       -> args { if :hi == args.first then args.shift and :hello end } ] ]
+  #
+  #     1  # => 1
+  #
+  # result array is in order of "grammar", not of elements in argv:
+  #
+  #     argv = [ :hi, 'BILLY', 'bob' ]
+  #     one, two = PARSER.call argv
+  #     one  # => 'BILLY'
+  #     two  # => :hello
+  #     argv # => [ 'bob' ]
+  #
+  # it cannot fail (if `set_a` is array of monadic functions and `argv` is ary)
+  #
+  #     argv = [ :nope ]
+  #     res = PARSER.call argv
+  #     res  # => [ nil, nil ]
+  #     argv # => [ :nope ]
+  #
+  # an unparsable element will "mask" subsequent would-be parsables:
+  #
+  #     argv = [ :nope, 'BILLY', :hi ]
+  #     res = PARSER.call argv
+  #     res  # => [ nil, nil ]
+  #     argv.length  # => 3
+  #
+
+  o[:parse_with_ordered_set] = -> set_a, argv do
+    len = set_a.length
+    running_a = len.times.to_a
+    res_a = ::Array.new len
+    while argv.length.nonzero? and running_a.length.nonzero?
+      index, res = running_a.each_with_index.reduce nil do |_, (idx, idx_idx)|
+        x = set_a.fetch( idx ).call argv
+        if ! x.nil?
+          running_a[ idx_idx ] = nil
+          running_a.compact!
+          break [ idx, x ]
+        end
+      end
+      index or break
+      res_a[ index ] = res
+    end
+    res_a
   end
 
   # `tuple_tower` - given a stack of functions and one seed value, resolve
--- lib/skylab/meta-hell/test/fun/generated-by-doc-test_spec.rb
+++ lib/skylab/meta-hell/test/fun/generated-by-doc-test_spec.rb
@@ -13,11 +13,56 @@ module Skylab::MetaHell::TestSupport::Fun
   Sandboxer = TestSupport::Sandbox::Spawner.new
 
   describe "Skylab::MetaHell::Fun" do
-    context "context 1" do
+    context "`parse_with_ordered_set` result is array of same length as `set_a`." do
       Sandbox_1 = Sandboxer.spawn
-      it "opaque but comprehensive example:" do
+      before :all do
         Sandbox_1.with self
         module Sandbox_1
+          PARSER = MetaHell::FUN.parse_with_ordered_set.curry[ [
+            -> args { args.shift if args.first =~ /bill/i },
+            -> args { if :hi == args.first then args.shift and :hello end } ] ]
+        end
+      end
+      it "like so" do
+        Sandbox_1.with self
+        module Sandbox_1
+          1.should eql( 1 )
+        end
+      end
+      it "result array is in order of \"grammar\", not of elements in argv" do
+        Sandbox_1.with self
+        module Sandbox_1
+          argv = [ :hi, 'BILLY', 'bob' ]
+          one, two = PARSER.call argv
+          one.should eql( 'BILLY' )
+          two.should eql( :hello )
+          argv.should eql( [ 'bob' ] )
+        end
+      end
+      it "it cannot fail (if `set_a` is array of monadic functions and `argv` is ary)" do
+        Sandbox_1.with self
+        module Sandbox_1
+          argv = [ :nope ]
+          res = PARSER.call argv
+          res.should eql( [ nil, nil ] )
+          argv.should eql( [ :nope ] )
+        end
+      end
+      it "an unparsable element will \"mask\" subsequent would-be parsables" do
+        Sandbox_1.with self
+        module Sandbox_1
+          argv = [ :nope, 'BILLY', :hi ]
+          res = PARSER.call argv
+          res.should eql( [ nil, nil ] )
+          argv.length.should eql( 3 )
+        end
+      end
+    end
+    context "`tuple_tower` - given a stack of functions and one seed value, resolve" do
+      Sandbox_2 = Sandboxer.spawn
+      it "opaque but comprehensive example" do
+        Sandbox_2.with self
+        module Sandbox_2
           f_a = [
             -> item do
               if 'cilantro' == item                 # the true-ishness of the 1st
