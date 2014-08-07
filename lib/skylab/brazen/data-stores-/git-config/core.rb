4b2eccccbfeb6274d457c65c1213558b64a4dcd7
Mark Meves <mark.meves@gmail.com>
2014-08-07 04:11:40 -0400

    (([br] DS git-config: parse section lines (( 103 )) )

--- lib/skylab/brazen/data-stores-/git-config/core.rb
+++ lib/skylab/brazen/data-stores-/git-config/core.rb
@@ -8,29 +8,95 @@ module Skylab::Brazen
       end
     end
 
+    ParseError = ::Class.new ::RuntimeError
+
     class Parse_Context__
       def with_input adapter_cls, x
+        @line_number = 0
         @lines = adapter_cls.new x
+        @parse_error_handler_p = nil
         self
       end
 
       def parse
         prepare_for_parse
         while @line = @lines.gets
+          @line_number += 1
           if BLANK_LINE_OR_COMMENT_RX__ =~ @line
           else
-            send @state_i
+            send @state_i or break
           end
         end
         @document
       end
+      BLANK_LINE_OR_COMMENT_RX__ = /\A[ ]*(?:\r?\n?\z|[#;])/
 
-      BLANK_LINE_OR_COMMENT_RX__ = /\A[ ]*(?:\z|[#;])/
+      # ~ support is coming first before business b.c possible future fission
+
+    private
+      def error_event i
+        x_a = [ i, :line_number, @line_number, :column_number, 1, :line, @line ]
+        ev = Brazen_::Entity::Event.new x_a, -> y, o do
+          y << "#{ i.to_s.gsub( UNDERSCORE_, SPACE_ ) } #{
+           }(#{ o.line_number }:#{ o.column_number })"
+        end
+        if @parse_error_handler_p
+        else
+          raise ParseError, ev.
+            render_first_line_under( Brazen_::API::EXPRESSION_AGENT )
+        end
+      end
+
+      # ~ business
 
       def prepare_for_parse
         @document = Document__.new
         @state_i = :when_before_section
       end
+
+      # ~
+
+      def when_before_section
+        @md = SECTION_RX__.match @line
+        if @md
+          accpt_section
+        else
+          whn_not_section
+        end
+      end
+      _NAME_RX = '[-A-Za-z0-9.]+'
+      SECTION_RX__ = /\A[ ]*\[[ ]*
+       (?<name>#{ _NAME_RX })
+        (?:[ ]+"(?<subsect>(?:[^\n"\\]+|\\["\\])+)"[ ]*)?\][ ]*\r?\n?\z/x
+
+      def accpt_section
+        if (( ss = @md[ :subsect ] ))
+          ss.gsub! BACKSLASH_QUOTE__, QUOTE__
+          ss.gsub! BACKSLASH_BACKSLASH__, BACKSLASH__
+        end
+        @sect = Section__.new @md[ :name ], ss
+        @document.sections.accept_sect @sect
+        @state_i = :when_section_or_assignment
+        PROCEDE__
+      end
+      BACKSLASH_QUOTE__ = '\\"'.freeze ; QUOTE__ = '"'.freeze
+      BACKSLASH_BACKSLASH__ = '\\\\'.freeze ; BACKSLASH__ = '\\'.freeze
+
+      def whn_not_section
+        error_event :section_expected
+      end
+
+      # ~
+
+      def when_section_or_assignment
+        if (( @md = SECTION_RX__.match @line ))
+          accpt_section
+        else
+          error_event :assignment_or_section_expected
+        end
+      end
+
+      PROCEDE__ = true ; CEASE__ = false
     end
 
     class String_Input_Adapter__
@@ -67,6 +133,27 @@ module Skylab::Brazen
       def length
         @a.length
       end
+      def first
+        @a.first
+      end
+      def map & p
+        @a.map( & p )
+      end
+      def accept_sect sect
+        @a.push sect ; nil
+      end
+    end
+
+    class Section__
+
+      def initialize name_s, subsect_name_s
+        @name_s = name_s.freeze
+        @subsect_name_s = ( subsect_name_s.freeze if subsect_name_s )
+        @normalized_name_i = @name_s.downcase.intern
+      end
+
+      attr_reader :name_s, :subsect_name_s
+      attr_reader :normalized_name_i
     end
   end
 end
--- lib/skylab/brazen/test/data-stores-/git-config-/core_spec.rb
+++ lib/skylab/brazen/test/data-stores-/git-config-/core_spec.rb
@@ -16,5 +16,53 @@ module Skylab::Brazen::TestSupport::Data_Stores_::Git_Config
       conf = Subject_[].parse_string str
       conf.sections.length.should be_zero
     end
+
+    it "some comments and one section parses" do
+      with <<-HERE.gsub! MARGIN_RX__, EMPTY_S_
+
+        # it's time
+        [scton]
+
+           ; wazoozle
+      HERE
+      expect_config do |conf|
+        conf.sections.length.should eql 1
+        conf.sections.first.normalized_name_i.should eql :scton
+      end
+    end
+
+    it "the subsection name parses" do
+      with '[ -.secto-2014.08 "foo \\" \\\\ " ]'
+      expect_config do |conf|
+        sect = conf.sections.first
+        sect.name_s.should eql '-.secto-2014.08'
+        sect.subsect_name_s.should eql 'foo " \\ '
+      end
+    end
+
+    it "two section names parse" do
+      with <<-HERE.gsub! MARGIN_RX__, EMPTY_S_
+        [ wiz "waz" ]
+        [WiZ]
+      HERE
+      expect_config do |conf|
+        conf.sections.map { |x| x.normalized_name_i }.should eql [ :wiz, :wiz ]
+      end
+    end
+
+    def with s
+      @input_string = s ; nil
+    end
+
+    def expect_config & p
+      conf = Subject_[].parse_string @input_string
+      if conf
+        block_given? ? yield( conf ) : conf
+      else
+        fail "expected config to parse, did not."
+      end
+    end
+
+    MARGIN_RX__ = /^[ ]{8}/
   end
 end
