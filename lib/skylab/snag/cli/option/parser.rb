254964f3e23fd0aab281b862eac8cc6b3e511416
Mark Meves <mark.meves@gmail.com>
2014-08-19 20:44:04 -0400

    ([sg] 2/7 de-functionalize option parser (( 75 )) )

--- lib/skylab/snag/cli/option/parser.rb
+++ lib/skylab/snag/cli/option/parser.rb
@@ -12,38 +12,45 @@ module Skylab::Snag
       super
     end
 
+    def regexp_replace_tokens rx, & p
+      ( @regexp_filters ||= [] ).push RX_Filter__.new rx, p ; nil
+    end
+    RX_Filter__ = ::Struct.new :rx, :block
+
     def parse! argv
-      if @regexp_filters
-        loop do
-          again = false
-          @regexp_filters.each_with_index do |filter, filter_idx|
-            token, idx = argv.each.with_index.detect { |t, i| filter.rx =~ t }
-            next if ! idx
-            orig_token = token.dup
-            res_a = filter.block.call $~, argv, idx
-            raise ::TypeError.new "filter blocks must result in Array, #{
-              }not #{ res_a.class }" unless ::Array === res_a
-            if orig_token == res_a.first
-              raise ::RuntimeError.new "filter would infinite loop, it #{
-              }did not change: #{ orig_token.inspect }"
-            end
-            argv[ idx, 1 ] = res_a
-            again = true
-            break # stop processing the rest of the filters, run all filters
-            # again on the new argv!
-          end
-          break if ! again
-        end
-      end
+      @regexp_filters and do_regexp_replace_tokens argv
       super
     end
+  private
+    def do_regexp_replace_tokens argv
+      begin
+        again = do_regexp_replace_tokens_once argv
+      end while again ; nil
+    end
 
-    rx_filter_struct = ::Struct.new :rx, :block
+    def do_regexp_replace_tokens_once argv
+      do_again = false
+      @regexp_filters.each_with_index do |filter, filter_idx|
+        token, idx = argv.each.with_index.detect { |t, i| filter.rx =~ t }
+        idx or next
+        orig_token = token.dup
+        res_a = filter.block.call $~, argv, idx
+        res_a.respond_to?( :each_with_index ) or raise ::TypeError, say_not( a )
+        orig_token == res_a.first and raise say_did_not_change( orig_token )
+        argv[ idx, 1 ] = res_a
+        do_again = true
+        break # stop processing the rest of the filters, run all filters
+        # again on the new argv!
+      end
+      do_again
+    end
 
-    define_method :regexp_replace_tokens do |rx, &block|
-      o = rx_filter_struct.new rx, block
-      ( @regexp_filters ||= [] ) << o
-      o
+    def say_not a
+      "filter blocks must result in Array, not #{ a.class }"
+    end
+
+    def say_did_not_change orig_token
+      "filter would infinite loop, it did not change: #{ orig_token.inspect }"
     end
   end
 end
