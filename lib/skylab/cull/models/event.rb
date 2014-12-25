8f49e5081d7d021ff38db5fd9ea5d4d63c0b9f40
Mark Meves <mark.meves@gmail.com>
2014-12-24 23:03:58 -0500

    ([cu]  1 of 10: renames/deletes..)
    Synopsis: we are ready to “cull” again.
    
     1. renames
     2. model ping
     3. survey ping
     4. survey status w/ nothing
     5. survey create
     6. survey status w/ something
     7. estream set (2 cases)
     8. estream unset (2 cases)
     9. criterion add (2 cases, 1 of which is mapping/scoring)
    10. criterion remove
    
    Changes in this commit: becuase of the superior technology
      of [br], there is no more need for these custom CLI and
      API modifications. get rid of a doffile fixtures because it
      is confusing when you don't see it (it's no longer covered
      anyway now that we are starting over.)

--- lib/skylab/cull/models/event.rb
+++
@@ -1,7 +0,0 @@
-module Skylab::Cull
-
-  Models::Event = Face::Model::Event
-
-  # (if this is not being used now it probably will be in the future.)
-
-end
--- lib/skylab/cull/test/cli/actions/test-support.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../test-support'
-
-module Skylab::Cull::TestSupport::CLI::Actions
-
-  ::Skylab::Cull::TestSupport::CLI[ self ]
-
-end
