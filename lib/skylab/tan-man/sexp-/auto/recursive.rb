875569e573eb48c4e25832ac9ae034e282451b8a
Mark Meves <mark.meves@gmail.com>
2015-02-03 00:56:06 -0500

    ([tm] 1 of 2: renames & deletes..)
    • 3x: canonically branch down or correct scope - the scope
      correction is based off of actual not designed visibility
    
    • ununsed node
    
    • 2x: both specs for "recursive rule" test mutation, none do not.
      but this spec unlike the other tests against production grammar,
      so it is more specific and more depending on basic things
      working. hence we move it downwards and move the other one
      upwards to take its place (sort of).

--- lib/skylab/tan-man/sexp-/auto/recursive.rb
+++
@@ -1,9 +0,0 @@
-module Skylab::TanMan
-  module Sexp_::Auto::Recursive
-    # this is a placeholder for the idea of it.  it may be just a more
-    # explicit alias of the thing.
-  end
-  module Sexp_::Auto::Recursive::BuildMethods ; include Sexp_::Auto::BuildMethods
-  end
-  Sexp_::Auto::Recursive.extend Sexp_::Auto::Recursive::BuildMethods
-end
