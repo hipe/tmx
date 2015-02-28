c88136f046c423eb9665c76963b0da33bbfebf1f
Mark Meves <mark.meves@gmail.com>
2015-02-27 21:18:52 -0500

    ([bs] G.U. API [fa] (1 of N) (1/3): delete 2 files..)
      • delete ANCIENT API action base class
    
      • this test support file will be assimilated

--- lib/skylab/beauty-salon/api/action.rb
+++
@@ -1,8 +0,0 @@
-module Skylab::BeautySalon
-
-  class API::Action < BS_.lib_.API_Action
-
-    taxonomic_streams  # none. (but this list (of none) is used to determine
-      # the list of unhandled non-taxonomic streams.)
-  end
-end
