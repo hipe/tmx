016ff50276f6afb48ebb90d3286301d5f75644d5
Mark Meves <mark.meves@gmail.com>
2015-07-09 13:14:41 -0400

    ([sy]       1/6: removes for FS n11n re-gel.. )
      • for all "modified" files here - will rewrite
    
      • (accompanying file for next)
    
      • the spirit behind this novelty ancient file is finally getting
        applied to this re-geling of the fs IO operations.
    
      • "nasty" tmpdir memoization is so common, we will push it up to
        tmpdir itself. so then remove the left behind orphan
        test support file.
    _

--- lib/skylab/system/services---/filesystem/normalization--/README.md
+++
@@ -1,4 +0,0 @@
-this is an amusing thing i found from a manual stash dated Feb 18, 2012
-
-its intent is obvious, but it is not at all working and certainly not
-up to standards.  nonetheless, there is something i like about it..
--- lib/skylab/system/services---/filesystem/normalization--/unlink-file--.rb
+++ lib/skylab/system/services---/filesystem/normalization--/unlink-file--.rb
@@ -7,58 +7,8 @@ module Skylab::System
 
         class Unlink_File__ < self
 
-          class << self
-            def mixed_via_iambic_ x_a
-              new do
-                process_polymorphic_stream_fully polymorphic_stream_via_iambic x_a
-                @x_a = @d = @x_a_length = nil  # #todo
-              end.produce_mixed_result_
-            end
-          end  # >>
+          # (will rewrite)
 
-          Callback_::Actor.methodic self, :simple, :properties,
-            :path,
-            :argument_arity, :zero, :if_exists,
-            :on_event_selectively
-
-          # #todo this is just a stub
-
-          def produce_mixed_result_
-            if @if_exists
-              when_if_exists
-            else
-              @x = ::File.unlink @path
-              via_x
-            end
-          end
-
-          def when_if_exists
-            @x = ::File.unlink @path
-            via_x
-          rescue ::Errno::ENOENT => @e
-            via_e
-          end
-
-          def via_e
-            @on_event_selectively.call :error, :enoent do
-              Callback_::Event.wrap.exception @e, :path_hack
-            end
-          end
-
-          def via_x
-            if 1 == @x  # the number of files in the arg, just a sanity check
-              when_success
-            else
-              UNABLE_
-            end
-          end
-
-          def when_success
-            @on_event_selectively.call :info, :success do
-              Callback_::Event.inline_neutral_with :deleted_file,
-                :path, @path
-            end
-          end
         end
       end
     end
