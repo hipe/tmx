1943084233ad60a2c1a602804cec36312c1b4c64
Mark Meves <mark.meves@gmail.com>
2016-05-01 16:25:40 -0400

    (interlude 1/3: REMOVES..)
    Note: we are removing *two* implementations of "pretty path" here,
    because in a subsequent commit we will rewrite it *fully* a second time..

--- system/test/services/filesystem/bridges/visual.rb
+++
@@ -1,9 +0,0 @@
-module Skylab::System
-
-  module TestSupport_Visual
-
-    class Services::Filesystem::Bridges < Branch_
-
-    end
-  end
-end
--- system/test/services/filesystem/visual.rb
+++
@@ -1,9 +0,0 @@
-module Skylab::System
-
-  module TestSupport_Visual
-
-    class Services::Filesystem < Branch_
-
-    end
-  end
-end
