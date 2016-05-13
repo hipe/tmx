771e4607a6b4c7f7867ba2cb80c1989af15df657
Mark Meves <mark.meves@gmail.com>
2016-05-13 14:51:32 -0400

    (m9 4/J: line scanner matures.. (( 30 )) )
      • subject certainly needed better coverage, esp near edge cases
    
      • while going to add the existence of the zero-width LTS,
        we were struck by the messy feeling of the non-structured,
        imperative style of programming.
    
      • decided the subject could stand to look more structured and
        get some self-optimization for whatever the predominant formal
        LTS is. (note that it is not probabalistic self-optimization,
        only last one wins but it would be trivial to change this.)
    _

--- search_and_replace/lib/skylab/search_and_replace/magnetics-/mutable-file-session-stream-via-file-session-stream/string-edit-session---/line-scanner-.rb
+++ search_and_replace/lib/skylab/search_and_replace/magnetics-/mutable-file-session-stream-via-file-session-stream/string-edit-session---/line-scanner-.rb
@@ -4,94 +4,162 @@ module Skylab::SearchAndReplace
 
     class String_Edit_Session___
 
-      class Line_Scanner_
+      class Line_Scanner_  # :[#011]:
+
+        # a minimal stream that produces "occurrence" structures, one
+        # occurrence for each newline sequence in the file (which for now
+        # must be represented as one big string (so this won't scale to
+        # large files)).
+        #
+        # each occurrence
+        #
+        #   • has the character position (not byte position) of the first
+        #     character of the newline sequence in the big string.
+        #
+        #   • knows the character width of the formal sequence.
+        #
+        # the subject will recognize any of the three known line termination
+        # formats, which we are calling "UNIX", "DOS" and "old mac". if the
+        # file employs a variety (eek) of formats, this will be detected and
+        # will be "self-optimized" for.
+        #
+        # if the file is not terminated by a line termination sequence (that
+        # is, if the file is zero bytes long or its last character is
+        # something other than the end of a line terminating sequence),
+        # a special zero-width occurence structure is produced as the last
+        # item in the stream so that the number of items produced by the
+        # stream is related to the number of "lines" in the file allowing
+        # that the last line is not well-formed (by the definition of line
+        # as suggested by the unix `wc` utility (more at [#sn-020]) or see
+        # `man git-log` near `tformat` & separator vs. terminator semantics).
+        #
+        # i.e, we still recognize "separator" semantics even though
+        # "terminator" semantics may be more formally correct. note this
+        # means that the zero-byte file is seen as having one "line" that
+        # has no terminator.
+        #
+        # NOTE this may change if we decide it is is logically more valuable
+        # to produce the empty stream in these cases, but this change would
+        # only ever pertain to the zero-byte file. in fact, at the moment it
+        # is more logically convenient to have it the way that it is
+        # currently, which we provisionally "lock" for ":#spot-3".
+
+        # the strscan idiom is fundamentally different than the regex idiom
+        # in this manner: strscan will never produce a matchdata; it only
+        # ever gives you matched strings or offsets (byte or character).
+        #
+        # as such, some things that are easy in regex make a bit more noise
+        # in strscan: either you match the same string sequence against the
+        # same regex twice (once for `StringScanner.scan` and a second time
+        # as plain old `Regexp.match` to get a matchdata yuck!), -OR- you
+        # write it out "longhand" as we do here.
+        #
+        # on the plus side, this way we conserve more memory because we
+        # don't build (on one of our larger typical jobs, for examle)
+        # tens of thousands of new strings whose only purpose is to tell
+        # us which newline sequence was used at that line WHEW!!
 
         def initialize string  # #testpoint
-          @_scn = Home_.lib_.string_scanner.new string
+
+          @_gets = :__first_gets
+          @_string = string
         end
 
         def gets
+          send @_gets
+        end
+
+        def __first_gets
+
+          s = remove_instance_variable :@_string
+          if s.length.zero?
+            @_gets = :_done
+            Zero_Width_Line_Terminator__.new 0
+          else
+            @_a = THESE___.dup
+            @_scn = Home_.lib_.string_scanner.new s
+            @_gets = :__main_gets
+            send @_gets
+          end
+        end
+
+        def __main_gets  # assume nonzero remainder
 
           @_scn.skip ONE_OR_MORE_NOT_LINE_SEPARATOR_CHARACTERS_RX___
 
-          # the strscan idiom is fundamentally different than the regex idiom
-          # in this manner: strscan will never produce a matchdata; it only
-          # ever gives you matched strings or offsets (byte or character).
-          #
-          # as such, some things that are easy in regex make a bit more noise
-          # in strscan: either you match the same string sequence against the
-          # same regex twice (once for `StringScanner.scan` and a second time
-          # as plain old `Regexp.match` to get a matchdata yuck!), -OR- you
-          # write it out "longhand" as we do here.
-          #
-          # on the plus side, this way we conserve memory better because we
-          # (on one of our larger refactors, for example) we aren't building
-          # tens of thousands of new strings whose only purpose is to tell
-          # us which newline sequence was used at that line WHEW!!
+          charpos_d = @_scn.charpos
 
-          # (per the above last `skip`, you are either at eos or head of etc)
-
-          d = @_scn.charpos
-
-          _d = @_scn.skip UNIX_NEWLINE_RX___
-
-          if _d
-
-            Unix_Line_Terminator___.new d
-
-          elsif @_scn.eos?
-
-            # (for now this method is "optimized" for unix format, and
-            # counter-optimized for others..)
-
-            NOTHING_
-
+          if @_scn.eos?
+            @_gets = :_done
+            Zero_Width_Line_Terminator__.new charpos_d
           else
-            _d = @_scn.skip WINDOWS_NEWLINE_RX___
-            if _d
-              Windows_Line_Terminator___.new d
-            else
-              _d = @_scn.skip OLD_MAC_NEWLINE_RX___
-              if _d
-                Old_Mac_Line_Terminator___.new d
-              else
-                self._COVER_ME_no_trailing_newline
-              end
+
+            cls_d = @_a.index do |cls|
+              @_scn.skip cls::RX
             end
+
+            if cls_d.nonzero?
+              tmp = @_a.fetch cls_d
+              @_a[ cls_d ] = @_a.fetch 0
+              @_a[ 0 ] = tmp
+            end
+
+            if @_scn.eos?
+              @_gets = :_done
+            end
+
+            @_a.fetch( 0 ).new charpos_d
           end
+        end
+
+        def _done
+          NOTHING_
         end
 
         # ==
 
         ONE_OR_MORE_NOT_LINE_SEPARATOR_CHARACTERS_RX___ = /[^\r\n]+/
-        UNIX_NEWLINE_RX___ = /\n/
-        WINDOWS_NEWLINE_RX___ = /\r\n/
-        OLD_MAC_NEWLINE_RX___ = /\r/
 
         Line_Terminator__ = ::Class.new
 
-        class Unix_Line_Terminator___ < Line_Terminator__
+        class UNIX___ < Line_Terminator__
+
+          RX = /\n/
+          S__ = "\n".freeze
 
           def sequence_width
             1
           end
         end
 
-        class Windows_Line_Terminator___ < Line_Terminator__
+        class DOS___ < Line_Terminator__
+
+          RX = /\r\n/
+          S__ = "\r\n".freeze
 
           def sequence_width
             2
           end
         end
 
-        class Old_Mac_Line_Terminator___ < Line_Terminator__
+        class Old_Mac___ < Line_Terminator__
+
+          RX = /\r/
+          S__ = "\r".freeze
 
           def sequence_width
             1
           end
         end
 
-        # Zero_Width_Line_Terminator___ < Line_Terminator__ ..
+        THESE___ =  [ UNIX___, DOS___, Old_Mac___ ]
+
+        class Zero_Width_Line_Terminator__ < Line_Terminator__
+
+          def sequence_width
+            0
+          end
+        end
 
         class Line_Terminator__
 
@@ -104,6 +172,14 @@ module Skylab::SearchAndReplace
           end
 
           attr_reader :charpos
+
+          def string
+            self.class.const_get :S__, false
+          end
+
+          def is_line_termination_sequence_
+            true
+          end
         end
       end
     end
--- search_and_replace/test/1-magnetics/01-line-scanner_spec.rb
+++ search_and_replace/test/1-magnetics/01-line-scanner_spec.rb
@@ -5,7 +5,7 @@ module Skylab::SearchAndReplace::TestSupport
   describe "[sa] magnetics - line scanner (multibyte bugfix)" do
 
     # all this time we've been happily trollomping along using *byte*
-    # offsets when we should be using *character* offsets. this never
+    # offsets when we should have be using *character* offsets. this never
     # reached out to bite us until we parsed files (strings) with multibyte
     # characters in them, like "«" and "»"
     # (#guillemets. reminder: option [shift] backslash.)
@@ -13,6 +13,29 @@ module Skylab::SearchAndReplace::TestSupport
     # this is so close to the issue of [#011] that we might..
 
     TS_[ self ]
+
+    it "one normal line" do
+      _to_a( "foo\n" ) == [ 'foo' ] or fail
+    end
+
+    it "one non-terminated line" do
+
+      p = _scan_proc_for 'foo'
+      no = p[]
+      p[] and fail
+      no.charpos == 3 or fail
+      no.end_charpos == 3 or fail
+    end
+
+    it "for now, empty file still sees it as having a final terminator" do
+
+      p = _scan_proc_for EMPTY_S_
+      no = p[]
+      p[] and fail
+
+      no.charpos.zero? or fail
+      no.end_charpos.zero? or fail
+    end
 
     it "don't bork over multibyte character sequences" do
 
@@ -26,32 +49,69 @@ module Skylab::SearchAndReplace::TestSupport
       _no4_ = p[]
       _no4_ and fail
 
-      r = -> no do
-        big_string[ no.charpos, no.sequence_width ]
+      p = _reader_of big_string
+
+      p[ no1 ] == NEWLINE_ or fail
+      p[ no2 ] == NEWLINE_ or fail
+      p[ no3 ] == NEWLINE_ or fail
+    end
+
+    it "multibyte, multi-line with no terminating sequence" do
+
+      hi = "«foo»\r\nbar"
+      p = _scan_proc_for hi
+      no1 = p[]
+      no2 = p[]
+      p[] and fail
+
+      no1.charpos == 5 or fail
+      no1.end_charpos == 7 or fail
+
+      no2.charpos == 10 or fail
+      no2.end_charpos == 10 or fail
+    end
+
+    it "change mojo mid-parse" do
+
+      a = _to_a "«foo»\r\nbar\r\r\rbaz\nboffo"
+      a[0] == "«foo»" or fail
+      a[1] == "bar" or fail
+      a[2].length.zero? or fail
+      a[3].length.zero? or fail
+      a[4] == "baz" or fail
+      a[5] == "boffo" or fail
+    end
+
+    def _to_a big_string
+      a = []
+      p = _scan_proc_for big_string
+      d = 0
+      begin
+        no = p[]
+        no or break
+        a.push big_string[ d ... no.charpos ]
+        d = no.end_charpos
+        redo
+      end while nil
+      a
+    end
+
+    def _scan_proc_for big_string
+
+      o = Magnetics::Build_line_scanner_[ big_string ]
+
+      # (saying `method( :gets )` instead of the below might accidentally
+      # hang blocking for IO, if we accidentally grab the private method)
+
+      -> do
+        o.gets
       end
-
-      r[ no1 ] == NEWLINE_ or fail
-      r[ no2 ] == NEWLINE_ or fail
-      r[ no3 ] == NEWLINE_ or fail
     end
 
-    once = -> cls do
-      once = nil
-      cls.public_method_defined?( :gets ) or fail
-    end
-
-    define_method :_scan_proc_for do |big_string|
-
-      cls = _subject_class
-      once && once[ cls ]
-      my_scn = cls.new big_string
-      my_scn.respond_to? :gets or fail  # sanity - don't block for IO
-      my_scn.method :gets
-    end
-
-    def _subject_class
-      Home_::Magnetics_::Mutable_File_Session_Stream_via_File_Session_Stream::
-          String_Edit_Session___::Line_Scanner_
+    def _reader_of s
+      -> no do
+        s[ no.charpos, no.sequence_width ]
+      end
     end
   end
 end
