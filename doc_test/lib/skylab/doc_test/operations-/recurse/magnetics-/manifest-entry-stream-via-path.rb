d847f49c029db5608ebd9c29fe9d4e5b6ea95f67
Mark Meves <mark.meves@gmail.com>
2016-08-23 11:18:23 -0400

    (01/R will rewrite..)
    "recurse" is getting the massivest of overhauls which will be the
    subject of the next perhaps 10 commits if no more.
    
    we *might* resuscitate "manifests"; we don't know yet. we're trying
    to get away from this in the spirit of convention over configuration
    (we've already knocked out most of the support code for the once-were
    "features" of manifests); BUT we're not sure yet whether we might
    bring them back but in conjunction with what we're doing SO a
    comfortable middle ground is to knock out the code but keep the
    files for now..

--- doc_test/lib/skylab/doc_test/operations-/recurse/magnetics-/manifest-entry-stream-via-path.rb
+++ doc_test/lib/skylab/doc_test/operations-/recurse/magnetics-/manifest-entry-stream-via-path.rb
@@ -1,33 +1,6 @@
-self._K
 module Skylab::DocTest
 
-  module DocTest
-
-    module Models_::Front
-
-      class Actions::Recursive
-
-        class Actors__::Produce_manifest_entry_stream
-
-          Attributes_actor_.call( self,
-            :path,
-            :doc_test_files_file,
-            :path_prop,
-          )
-
-          def initialize
-            @filesystem = Home_.lib_.system.filesystem
-            super
-          end
-
-          def execute
-
-            ok = __resolve_manifest_path
-            ok &&= __via_manifest_path_resolve_open_file
-            ok && __build_result
-          end
-
-          def __resolve_manifest_path
+  self._K_will_rewrite
 
             start_path = if ::File.file? @path  # walks start from dirs always
               ::File.dirname @path
@@ -59,16 +32,6 @@ module Skylab::DocTest
 
             @open_file_IO && ACHIEVED_
           end
-
-          def __build_result
-
-            Result___.new @open_file_IO, @manifest_path, @surrounding_path
-          end
-
-          Result___ = ::Struct.new :open_file_IO, :manifest_path, :surrounding_path
-        end
-      end
-    end
-  end
 end
+# #tombstone: rewrite to simplify away manifest file syntax
 # :+#tombstone: back when the search was expressed by dir and file
