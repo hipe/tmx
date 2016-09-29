27f437b0cf69ecee7aa2154e0ef2a4f0eb200e47
Mark Meves <mark.meves@gmail.com>
2016-09-29 04:29:51 -0400

    (2/J [tm] stack-viz one-off - stub the graph.)

--- tan_man/lib/skylab/tan_man/stack-magnetics-/graph-via-item-stream.rb
+++ tan_man/lib/skylab/tan_man/stack-magnetics-/graph-via-item-stream.rb
@@ -3,11 +3,67 @@ module Skylab::TanMan
   class StackMagnetics_::Graph_via_ItemStream < Common_::Actor::Monadic
 
     def initialize st
-
+      @_graph = Graph___.new
     end
 
     def execute
-      :_graph_not_yet_read_
+      ok = true
+      ok && @_graph
+    end
+
+    # ==
+
+    class Graph___
+
+      def initialize
+        a = []
+        a.push Association___.new( :A_node, :B_node )
+        a.push Association___.new( :C_node, :B_node )
+        @_associations = a
+        a = []
+        a.push Item___.new( :A_node, "the A node" )
+        a.push Item___.new( :B_node, "the B node" )
+        a.push Item___.new( :C_node, "the C node" )
+        @_items = a
+      end
+
+      def to_association_stream
+        Common_::Stream.via_nonsparse_array @_associations
+      end
+
+      def to_item_stream
+        Common_::Stream.via_nonsparse_array @_items
+      end
+    end
+
+    # ==
+
+    class Item___
+
+      def initialize sym, s
+        @item_label = s
+        @item_symbol = sym
+      end
+
+      attr_reader(
+        :item_label,
+        :item_symbol,
+      )
+    end
+
+    # ==
+
+    class Association___
+
+      def initialize fr_sym, to_sym
+        @from_symbol = fr_sym
+        @to_symbol = to_sym
+      end
+
+      attr_reader(
+        :from_symbol,
+        :to_symbol,
+      )
     end
   end
 end
--- tan_man/lib/skylab/tan_man/stack-magnetics-/line-stream-via-graph.rb
+++ tan_man/lib/skylab/tan_man/stack-magnetics-/line-stream-via-graph.rb
@@ -3,7 +3,7 @@ module Skylab::TanMan
   class StackMagnetics_::LineStream_via_Graph < Common_::Actor::Monadic
 
     def initialize g
-
+      @_graph = g
     end
 
     def execute
@@ -15,17 +15,48 @@ module Skylab::TanMan
 
     def __open
       @_method = :__first_body_line
-      "digraph g {\n"
+      "digraph g {#{ NEWLINE_ }"
     end
 
     def __first_body_line
-      @_method = :__last_line
-      "  a->b\n"
+
+      @_item_stream = @_graph.to_item_stream
+      @_method = :__next_item
+      send @_method
     end
 
-    def __last_line
-      @_method = :__done
-      "}\n"
+    def __next_item
+      item = @_item_stream.gets
+      if item
+        __render_item_line item
+      else
+        remove_instance_variable :@_item_stream
+        @_association_stream = @_graph.to_association_stream
+        @_method = :__next_association
+        send @_method
+      end
+    end
+
+    def __render_item_line item
+
+      item_sym = item.item_symbol
+      s = item.item_label
+      # ..
+      escaped_item_label = s
+
+      # --
+
+      "  #{ item_sym } [label=\"#{ escaped_item_label }\"]#{ NEWLINE_ }"
+    end
+
+    def __next_association
+      asso = @_association_stream.gets
+      if asso
+        "  #{ asso.from_symbol }->#{ asso.to_symbol }#{ NEWLINE_ }"
+      else
+        @_method = :__done
+        "}#{ NEWLINE_ }"
+      end
     end
 
     def __done
