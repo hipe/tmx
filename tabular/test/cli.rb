773c0e3bcdf69f8c0000d5f670a0e57fae182e21
Mark Meves <mark.meves@gmail.com>
2016-12-02 17:06:18 -0500

    ((       [tab] WILL REWRITES. ))

--- tabular/test/cli.rb
+++ tabular/test/cli.rb
@@ -4,27 +4,7 @@ $LOAD_PATH.unshift(root) unless $LOAD_PATH.include?(root)
 require 'assess'
 
 class MiniTest::Unit::TestCase
-  def assert_superset(expected, actual, msg=nil)
-    missing = expected - actual
-    if missing.any?
-      msg = "#{msg} missing keys: (#{missing.join(', ')})"
-      assert false, msg
-    else
-      assert true, msg
-    end
-  end
-  def assert_equal_set(expected, actual, msg=nil)
-    missing = expected - actual
-    extra   = actual - expected
-    if ! missing.any? && ! extra.any?
-      assert true
-    else
-      if missing.any?
-        assert(false, "required #{msg} missing: (#{missing.join(', ')})")
-      end
-      if extra.any?
-        assert(false, "unexpected #{msg}: (#{extra.join(', ')})")
-      end
-    end
-  end
+
+  # will rewrite.
 end
+# #tombstone: full reconception from ancient [as]
