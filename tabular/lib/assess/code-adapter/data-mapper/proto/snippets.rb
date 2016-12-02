533c89e1b8b881c5215ff9da07b7b0d8819f0df8
Mark Meves <mark.meves@gmail.com>
2016-12-02 17:38:21 -0500

    ((       [tab] archive all data-mapper related ))

--- tabular/lib/assess/code-adapter/data-mapper/proto/snippets.rb
+++
@@ -1,8 +0,0 @@
-module ModelCommon
-  def self.included(model)
-    model.class_eval do
-      include DataMapper::Resource
-      property :id, DataMapper::Types::Serial, :required => true
-    end
-  end
-end
