461b8e5f3484e3d33de4366dd8ee3a4b8e52c851
Mark Meves <mark.meves@gmail.com>
2017-01-19 02:28:29 -0500

    (-3.165 [pl] will rewrite.. )
    we're chosing this oldest of plugin nodes to house the center
    of our massive simplification of plugins..
    
    we will repurpose this oldest of its test files to house the
    conceptual replacement of pub-sub..

--- basic/test/222-queue/2-core_spec.rb
+++ basic/test/222-queue/2-core_spec.rb
@@ -7,74 +7,7 @@ module Skylab::Basic::TestSupport
     TS_[ self ]
     use :queue
 
-    memoize_ :subject_class_ do
-
-      class Q_AQ_Args_Cls_01
-
-        include TS_::Queue::Methods_to_Make_a_Client_Class_Testable
-
-        # <-
-
-      def honka *a
-        _push :HNKA, * a
-      end
-
-      def tonka
-        _push :tnka
-      end
-
-      def flonka one, two=nil, three
-        two ||= :TWO
-        _push :flnk, one, two, three
-      end
-
-      def wonka x
-        _push x
-        if :three == x
-          7
-        else
-          0
-        end
-      end
-
-      self
-      # ->
-      end
-    end
-
-    it "loads" do
-
-      subject_module_
-    end
-
-    it "passes along any args to a method" do
-
-      action.enqueue_with_args :honka, :cream, :pie
-      invoke
-      @action.a.should eql %i( HNKA cream pie )
-      @result.should be_zero
-    end
-
-    it "will call all the methods bound in the queue at call time" do
-
-      action.enqueue :tonka
-      @action.enqueue_with_args :flonka, :deef, :dorf
-      @action.enqueue_with_args :honka
-      invoke
-      @action.a.should eql %i( tnka flnk deef TWO dorf HNKA )
-      @result.should be_zero
-    end
-
-    it "but will short circuit if ever a method does not result in true" do
-
-      action.enqueue_with_args :wonka, :one
-      @action.enqueue_with_args :wonka, :two
-      @action.enqueue_with_args :wonka, :three
-      @action.enqueue_with_args :wonka, :four
-      @action.enqueue_with_args :wonka, :five
-      invoke
-      @action.a.should eql %i( one two three )
-      @result.should eql 7
-    end
+    # (will rewrite)
   end
 end
+# #tombstone-A: rewrite of [br]-era queue
--- plugin/test/92-dependencies/40-pub_sub_spec.rb
+++ plugin/test/92-dependencies/40-pub_sub_spec.rb
@@ -4,70 +4,7 @@ module Skylab::Plugin::TestSupport
 
   describe "[pl] dependencies - pub-sub" do
 
-    TS_[ self ]
-    use :dependencies
-
-    before :all do
-
-      module De_Ps1
-
-        class C1
-          ROLES = nil
-          SUBSCRIPTIONS = [ :carbonate, :tangle ]
-        end
-
-        class C2
-          ROLES = nil
-          SUBSCRIPTIONS = nil
-        end
-
-        class C3
-          ROLES = nil
-          SUBSCRIPTIONS = [ :miff, :carbonate ]
-        end
-      end
-    end
-
-    it "every subscriber receives it" do
-
-      carb_a = []
-
-      _disp.accept_by :carbonate do | de |
-        carb_a.push de.class
-        ACHIEVED_
-      end
-
-      2 == carb_a.length or fail
-      carb_a.should be_include De_Ps1::C1
-      carb_a.should be_include De_Ps1::C3
-    end
-
-    it "you can short circuit it by calling `break`" do
-
-      seen_count = 0
-
-      _disp.accept_by :carbonate do | de |
-
-        seen_count += 1
-        break
-      end
-
-      seen_count.should eql 1
-    end
-
-    it "if an emission is sent on a channel with no subscribers, nothing" do
-
-      _disp.accept_by :warbonate do | de |
-        fail
-      end
-    end
-
-    dangerous_memoize :_disp do
-
-      o = subject_class_.new
-      o.emits = [ :carbonate, :miff, :tangle, :warbonate ]
-      o.index_dependencies_in_module De_Ps1
-      o
-    end
+    # (will rewrite)
   end
 end
+# #tombstone-A: full rewrite
