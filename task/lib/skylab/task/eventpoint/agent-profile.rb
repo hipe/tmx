648f165023f69cb249dee586aad6ad5dd0d6d177
Mark Meves <mark.meves@gmail.com>
2017-01-20 16:06:52 -0500

    (-3.161 [ta] will rewrite 1x)

--- task/lib/skylab/task/eventpoint/agent-profile.rb
+++ task/lib/skylab/task/eventpoint/agent-profile.rb
@@ -1,56 +1,10 @@
-module Skylab::Plugin
+class Skylab::Task
 
-  class Digraphic  # :[#004] (see)
+  class Eventpoint::AgentProfile
 
-    class Base_Dispatcher___  # meh - this used to be in our topmost ..
+    # ==
 
-      # file but wans't shared. we have not yet un-abstracted it fully
-
-      def initialize resources, & oes_p
-        @on_event_selectively = oes_p
-        @plugin_a = []
-        @resources = resources
-      end
-
-      undef_method :initialize_dup
-
-      def load_plugins_in_module mod
-
-        _st = Common_::Stream.via_nonsparse_array mod.constants do | const |
-
-          mod.const_get const, false
-
-        end
-
-        load_plugins_in_prototype_stream _st
-      end
-
-      def load_plugins_in_prototype_stream st
-
-        st.each do | plugin_class_like |
-
-          add_plugin_via_prototype plugin_class_like
-        end
-        NIL_
-      end
-
-      def add_plugin_via_prototype plugin_class_like
-
-        pu_d = @plugin_a.length
-
-        de = plugin_class_like.new_via_plugin_identifier_and_resources(
-          pu_d, @resources, & @on_event_selectively )
-
-        receive_plugin pu_d, de
-        NIL_
-      end
-
-      def receive_plugin pu_d, de
-
-        @plugin_a[ pu_d ] = de
-        NIL_
-      end
-    end
+    Base_Dispatcher___ = ::Class.new  # (dusted, here for now to keep the below legacy)
 
     class Dispatcher < Base_Dispatcher___
 
@@ -68,32 +22,6 @@ module Skylab::Plugin
       def state_machine * tuples
         @state_machine = State_Machine___.new tuples
         nil
-      end
-
-      def create_plugin_via_option_parser name_symbol
-
-        pu_d = @plugin_a.length
-
-        de = Mutable___.new_via_name_and_plugin_identifier_and_resources(
-          Common_::Name.via_variegated_symbol( name_symbol ),
-          pu_d,
-          @resources )
-
-        reac = Reaction__.new name_symbol
-
-        sess = Formal_Edit_Session__.new [ name_symbol ] do | fo |
-
-          reac.ancillary_formals.push fo
-          NIL_
-        end
-
-        yield sess
-
-        de.reactions.push reac
-
-        _accept_plugin pu_d, de
-
-        de
       end
 
       def receive_plugin pu_d, de
@@ -128,39 +56,6 @@ module Skylab::Plugin
         super
       end
 
-      # ~ ( for collaborators
-
-      attr_reader :all_capabilities, :occurrence_a, :plugin_a
-
-      def digraph
-        @state_machine.digraph
-      end
-
-      def accept & yld_p
-        @_monitor.accept( & yld_p )
-        NIL_
-      end
-
-      # ~ )
-
-      def _register_occurrence formal, tr_i, pu_d, is_catalyst=nil
-
-        # group all occurrences of all formals in all plugins by
-        # however the monitor wants to group them..
-
-        occu_idx = @occurrence_a.length
-        occu = Occurrence__.new occu_idx, formal, pu_d, tr_i, is_catalyst
-
-        @_monitor.register_occurrence occu
-
-        @occurrence_a[ occu_idx ] = occu
-
-        NIL_
-      end
-
-      Occurrence__ = ::Struct.new :occurrence_index, :formal, :plugin_idx,
-        :transition_symbol, :is_catalyst
-
       def bound_call_via_ARGV input_x  # [pl]
 
         Me_::Modality_Adapters_::ARGV::Produce_bound_call.new(
@@ -171,63 +66,6 @@ module Skylab::Plugin
       end
 
       # ~ dispatcher ancillaries
-
-      class State_Machine___
-
-        # (this was a mentor to #[#ba-044.2]. the student has surpassed the teacher)
-
-        def initialize tuples
-          @state_symbol = :started
-          @digraph = State_Machine_Digraph___.new tuples
-        end
-
-        attr_reader :digraph, :state_symbol
-
-        def accept_transition tr_sym
-          @state_symbol = @digraph.target_state_of_transition tr_sym
-          nil
-        end
-      end
-
-      class State_Machine_Digraph___
-
-        def initialize tuples
-          @a = ::Array.new tuples.length / 3
-          @h = {}
-          d = -1
-          tuples.each_slice 3 do | sym_a |
-            d += 1
-            tr = Transition___.new d, * sym_a
-            @a[ d ] = tr
-            @h[ tr.name_symbol ] = tr
-          end
-        end
-
-        def source_state_of_transition tr_sym
-          @h.fetch( tr_sym ).from_symbol
-        end
-
-        def target_state_of_transition tr_sym
-          @h.fetch( tr_sym ).to_symbol
-        end
-
-        def transition tr_sym
-          @h.fetch tr_sym
-        end
-      end
-
-      class Transition___
-
-        def initialize * a
-          @transition_index, @from_symbol, @name_symbol, @to_symbol = a
-        end
-
-        attr_reader :transition_index, :from_symbol, :name_symbol, :to_symbol
-
-        def name
-          @__name__ ||= Common_::Name.via_variegated_symbol( @name_symbol )
-        end
-      end
 
       class Step
 
@@ -432,6 +270,8 @@ module Skylab::Plugin
       end
     end  # Dispatcher
 
+    # ==
+
     # ~ plugin exposures for collaborators
 
     class << self
@@ -441,201 +281,7 @@ module Skylab::Plugin
       end
     end  # >>
 
-    # ~ plugin as base class
-
-    class << self
-
-      def inherited cls
-        cls.__inherited
-        nil
-      end
-
-      protected def __inherited
-        @_cap_a = nil
-        @_reac_a = nil ; nil
-      end
-
-      attr_reader :_cap_a, :_reac_a
-
-      def does transition_symbol, & dsl_p
-
-        tr = Reaction__.new transition_symbol
-        dsl_p[ tr ]
-        ( @_reac_a ||= [] ).push tr
-        NIL_
-      end
-
-      def can transition_symbol, & dsl_p
-        tr = Capability__.new transition_symbol
-        if dsl_p
-          dsl_p[ tr ]
-        end
-        ( @_cap_a ||= [] ).push tr
-        nil
-      end
-
-      alias_method :new_via_plugin_identifier_and_resources, :new
-      private :new
-    end  # >>
-
-    # ~ ancillaries for plugin capabilities specification
-
-    class Capability__
-
-      def initialize sym
-        @ancillary_formals = []
-        @transition_symbol = sym
-      end
-
-      attr_reader :ancillary_formals, :transition_symbol
-
-      def each_ancillary_formal_option & yld_p
-        a = @ancillary_formals
-        if a
-          a.each( & yld_p ) ; nil
-        end
-      end
-
-      def if_transition_is_effected & op_p
-
-        each_option_in_as_(
-          :ancillary_formals,
-          @transition_symbol,
-          my_symbol_,
-          op_p
-        ) do | op |
-
-          @ancillary_formals.push op ; nil
-        end
-
-        NIL_
-      end
-
-      def my_symbol_
-        :capabilities
-      end
-
-      def each_option_in_as_ * id_a, op_p, & op_o_p
-        op_p[ Formal_Edit_Session__.new( id_a, & op_o_p ) ]
-        nil
-      end
-    end
-
-    class Reaction__ < Capability__
-
-      def initialize( * )
-        @catalyzing_formals = nil
-        super
-      end
-
-      attr_reader :catalyzing_formals
-
-      def each_catalyzing_formal & yld_p
-        a = @catalyzing_formals
-        if a
-          a.each( & yld_p ) ; nil
-        end
-      end
-
-      def transition_is_effected_by & op_p
-
-        each_option_in_as_(
-          :catalyzing_formals,
-          @transition_symbol,
-          my_symbol_,
-          op_p
-        )do | op |
-
-          ( @catalyzing_formals ||= [] ).push op ; nil
-        end
-        NIL_
-      end
-
-      def my_symbol_
-        :reactions
-      end
-    end
-
-    class Formal_Edit_Session__
-
-      def initialize id_prefix_x, & p
-        @id_prefix_x = id_prefix_x
-        @d = -1
-        @op_o_p = p
-      end
-
-      def on * a, & any_p
-        @op_o_p[ Me_::Modality_Adapters_::ARGV::Formal.new a,
-          [ ( @d += 1 ), * @id_prefix_x ],
-          & any_p ]
-        nil
-      end
-    end
-
-    # ~ plugin as base class (instance methods)
-
-    def initialize plugin_index, resources, & oes_p
-      @on_event_selectively = oes_p
-      @plugin_index = plugin_index
-      @resources = resources
-    end
-
-    attr_reader :plugin_index
-
-    def name
-      @nm ||= Common_::Name.via_module self.class
-    end
-
-    def each_reaction & yld_p
-      a = reactions
-      if a
-        a.each( & yld_p )
-      end
-      nil
-    end
-
-    def each_capability & yld_p
-      a = capabilities
-      if a
-        a.each( & yld_p )
-      end
-      nil
-    end
-
-    def formal_via_formal_path_ formal_path_x
-
-      opt_id, which, tr, c_or_r = formal_path_x
-
-      send( c_or_r ).detect do | cap |
-        tr == cap.transition_symbol
-      end.send( which ).fetch opt_id  # #sorry
-    end
-
-    def reactions
-      self.class._reac_a
-    end
-
-    def capabilities
-      self.class._cap_a
-    end
-
-    class Mutable___ < self
-
-      class << self
-
-        alias_method :new_via_name_and_plugin_identifier_and_resources, :new
-        public :new_via_name_and_plugin_identifier_and_resources
-      end  # >>
-
-      def initialize name, * rest
-        @reactions = []
-        @name = name
-        super( * rest )
-      end
-
-      attr_reader :reactions, :name
-    end
-
-    Me_ = self
+    # ==
   end
 end
+# :#tombstone-A: (could be temporary) remove legacy code we are about to rewrite
