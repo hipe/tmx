9ff1fa6ea4d0d3ac1b5e22154201c338f213ba5d
Mark Meves <mark.meves@gmail.com>
2017-07-15 08:10:28 -0400

    (-17.981 -5 "reallocate sigils" one off! (new algorithm))
    (this is committed here as a snapshot, so that the changes made
    to the test file in an upcoming commit are reflected in history,
    in case we weirdly want to go back.)

--- tmx/lib/skylab/tmx/models/sigil.rb
+++ tmx/lib/skylab/tmx/models/sigil.rb
@@ -2,6 +2,33 @@ module Skylab::TMX
 
   module Models::Sigil
 
+    # a "sigil" is simply the (so far always) 2- or 3-character wide short
+    # name we use to identify (uniquely) a sidesystem within the context of
+    # all sidesystems in the tmx ecosystem. for example, the sigil for
+    # "common" is "[co]". the sigil for "tmx" is "[tmx]".
+    #
+    #   - for whatever reason, we always use the brackets ("[]") around
+    #     sigil names when they appear in print. (but their internal
+    #     representation probably will not have the brackets in the
+    #     string/symbol.)
+    #
+    #   - we would like that the sigils identify the sidesystem uniquely
+    #     not just in the context of all sidesystems installed on the
+    #     system but rather all sidesystems known to exist in the real
+    #     universe for that version of the ecosystem.
+
+    # at a time in the past (#history-B), we were able to derive the
+    # "sigilation" entirely algorithmically as discussed #here-1.
+    # however this approach had signifcant costs:
+    #
+    #   - it became hard to make the algorithm match our aesthetics.
+    #     every next new name of a sidesystem seemed to require that we
+    #     change not just our list of rules but our algorithm so that the
+    #     desired sigil(s) would be generated.
+    #
+    #   - as algorithms had been written to date, they depended what
+    #     sidesystems were actually installed
+
     # this node is classified as a "model" for taxonomics and future-
     # proofing. it's really just a functions node for functions that produce
     # a collection of "sigilizations" from a collection of various shapes of
@@ -16,347 +43,360 @@ module Skylab::TMX
       end
 
       def via_stemish_stream x
-        o = Index___.new
-        o.stemish_stream = x
-        o.execute
+        Index__.call_by do |o|
+          o.stemish_stream = x
+        end
       end
 
       def via_stemish_box bx
-        o = Index___.new
-        o.stemish_box = bx
-        o.execute
+        Index__.call_by do |o|
+          o.stemish_box = bx
+        end
       end
     end  # >>
 
-    class Anything___
+    # ==
+
+    class Result___
 
       # encapsulate the internal representation of the full sigilization
       # effort, and create various collection shapes from it on-demand.
 
-      def initialize bx
-        @_box = bx
+      def initialize sigils, d, items
+        @items = items
+        @length_of_longest_entry_string = d
+        @sigils = sigils
+        freeze
       end
 
-      def to_stream
-
-        # after a performance below we are left with a box keyed to valid
-        # sigils, whose each value is an array of "sigilizations". because
-        # the below performer (that build the subject object) succeeded, we
-        # must assume there is exactly one sigilization per sigil. we don't
-        # bother to map that array down to the item until it is requested;
-
-        @_box.to_value_stream.map_by do | a |
-          a.fetch 0
+      def to_stream  # NOTE - allocates new memory each time
+        sigils = @sigils ; items = @items
+        Common_::Stream.via_times items.length do |d|
+          Sigilization___.new sigils.fetch( d ), :_DO_ME_EASY_tmx_, items.fetch( d )
         end
       end
+
+      attr_reader(
+        :length_of_longest_entry_string,
+      )
     end
 
-    class Index___
+    # ==
+
+    class Index__ < Common_::MagneticBySimpleModel
 
       # find the shortest distinct "sigil" for each sidesystem using a
-      # variety (3 count) of heuristic strategies to shorten sidesystem names.
+      # variety of heuristic strategies to shorten sidesystem names.
+      #
+      # the authoritative reference to our algorithm *is* the `execute`
+      # method, which is meant to be readable as pseudocode.
       #
       # (this became a #case-study (see tombstone at end))
       #
-      # we don't need to resolve conflicts if the client is looking for the
-      # sigil of a sidesystem that doesn't conflict after the first pass but
-      # we resolve all conflicts anyway because meh.
-      #
-      # some of the old name for sigil ("medallion") is still used here and
-      # is left intact, perhaps because it is useful as a differentiator for
-      # a sigil associated with a sidesystem as opposed to one that is not.
+      # :#here-1
 
       def initialize
         @stemish_stream = nil
-        @stemish_box = nil
+        super
+
+        @_cha_cha_table = Common_::Box.new
+        @_entry_string_via_offset = []
+        @_length_of_longest_whatever = 0
+        @_multiple_pieces_bucket = []
+        @_offset_via_sigil_box = Common_::Box.new
+        @_one_piece_bucket = []
+        @_pieces_via_offset = []
+        @_remote_items = []
       end
 
-      def stemish_stream= x
-        @stemish_stream = x
-        x
+      def stemish_box= bx
+        @stemish_stream = bx.to_value_stream
+        bx
       end
 
-      def stemish_box= x
-        @stemish_box = x
-        x
-      end
+      attr_writer(
+        :stemish_stream,
+      )
 
       def execute
 
-        if ! @stemish_stream
-          @stemish_stream = @stemish_box.to_value_stream
-        end
-
-        @_box = Common_::Box.new
-        @_conflicts = nil
-
-        ___for_the_first_pass_use_the_two_initials_function
-
-        if @_conflicts
-          _resolve_conflicts_using :__second_strategy
-        end
-
-        if @_conflicts
-          _resolve_conflicts_using :__third_strategy
-        end
-
-        if @_conflicts
-          self._DESIGN_ME
-        else
-          Anything___.new @_box
-        end
-      end
-
-      def ___for_the_first_pass_use_the_two_initials_function
-
-        _st = remove_instance_variable :@stemish_stream
-
-        _st.each do | up_x |
-          _meda = ___build_medallion_with_two_initials up_x
-          __add_medallion _meda
-        end
-        NIL_
-      end
-
-      def ___build_medallion_with_two_initials up_x
-
-        pieces = up_x.entry_string.split SPLITTER_RX___, 2
-
-        _very_short_string = if 1 == pieces.length
-          pieces.first[ 0, 2 ]
-        else
-          pieces.map { |s| s[ 0 ] }.join EMPTY_S_
-        end
-
-        Sigilization___.new _very_short_string, pieces, up_x
-      end
-
-      SPLITTER_RX___ = /(?:_|(?=[0-9]))/
-
-      def __add_medallion meda
-
-        a = @_box.touch_array meda.sigil
-        a.push meda
-        if 2 == a.length
-          _a_ = ( @_conflicts ||= [] )
-          _a_.push meda.sigil
-        end
-        NIL_
-      end
-
-      def __second_strategy very_short_string
-
-        # if the numbers of pieces are the same among all the conflicting
-        # medallions, find the first piece that is not the same and use 2
-        # letters not one oh my.
-        #
-        # 'CodeMolester' vs. 'CodeMetrics' => [cmo] [cme]
-
-        meda_a = @_box.fetch very_short_string
-
-        _number_of_distinct_numbers_of_pieces =
-          meda_a.reduce( {} ) { |h, o| h[ o.pieces.length ] = nil ; h }.length
-
-        if 1 == _number_of_distinct_numbers_of_pieces
-
-          _number_of_pieces = meda_a.first.pieces.length  # same for all
-
-          is_this_piece_distinct_among_the_medallions = -> d do
-            seen = {}
-            meda_a.reduce nil do | _, o |
-              s = o.pieces.fetch( d )
-              if seen[ s ]
-                break false
-              else
-                seen[ s ] = true
-                true
-              end
-            end
-          end
-
-          use_this_d = _number_of_pieces.times.reduce nil do | _, d |
-
-            _yes = is_this_piece_distinct_among_the_medallions[ d ]
-            if _yes
-              break d
-            end
-          end
-
-          if use_this_d
-            ___attempt_second_strategy_on use_this_d, very_short_string
-          end
-        end
-      end
-
-      def ___attempt_second_strategy_on use_d, bad_very_short_string
-
-        meda_a = @_box.fetch bad_very_short_string
-
-        create = -> meda do
-
-          pieces = meda.pieces
-
-          sub_pieces = use_d.times.map do | d |
-            pieces.fetch( d )[ 0, 1 ]  # only one letter here
-          end
-
-          sub_pieces.push pieces.fetch( use_d )[ 0, 2 ]  # two letters here
-
-          sub_pieces.join EMPTY_S_
-        end
-
-        made = []
-        ok = true
-        seen = {}
-
-        meda_a.each_with_index do | meda, d |
-
-          new_very_short = create[ meda ]
-
-          if seen[ new_very_short ]
-            ok = false
-            break
-          end
-
-          if @_box.has_key new_very_short
-            ok = false
-            break
-          end
-
-          seen[ new_very_short ] = true
-
-          made[ d ] = new_very_short
-        end
-
-        if ok
-
-          _accept_new_very_short_strings made, bad_very_short_string
-          ACHIEVED_
-        else
-          UNABLE_
-        end
-      end
-
-      def __third_strategy bad_very_short_string
-
-        # if the medallion has one piece, use the first three letters of it
-        # (we can increase this as needed perhaps). otherwise use the first
-        # letter of each piece.
-
-        # 'TMX' vs. 'TanMan' => [tmx] [tm]
-
-        create = -> meda do
-
-          if 1 == meda.pieces.length
-            meda.pieces.fetch( 0 )[ 0, 3 ]
+        while __next_item
+          if __current_thing_ding_is_exactly_two_or_three_characters_long
+            __use_that
+          elsif __the_thing_ding_has_multiple_pieces
+            __put_it_in_the_multiple_pieces_bucket
           else
-            meda.pieces.map{ |s| s[0] }.join EMPTY_S_
+            __put_it_in_the_one_piece_bucket
           end
         end
 
-        meda_a = @_box.fetch bad_very_short_string
-
-        made = []
-        ok = true
-        seen = {}
-
-        meda_a.each_with_index do | meda, d |
-
-          new_very_short = create[ meda ]
-
-          if seen[ new_very_short ]
-            ok = false
-            break
-          end
-
-          if bad_very_short_string != new_very_short
-
-            # it's allowable for us to re-produce the original "bad" very
-            # short again here by this algorithm and have it be OK because
-            # of the check above that we aren't using it more than once.
-            # it remains a potentially ambiguous identifier, but the gravity
-            # to keep sigils down to 2 chars is stronger than the gravity to
-            # avoid ambiguity (by this code).
-
-            if @_box.has_key new_very_short
-              ok = false
-              break
-            end
-          end
-
-          seen[ new_very_short ] = true
-          made[ d ] = new_very_short
+        while __next_item_in_the_multiples_bucket
+          __use_the_name_derived_from_each_first_character_of_each_piece
         end
 
-        if ok
-          _accept_new_very_short_strings made, bad_very_short_string
-          ACHIEVED_
+        while __next_item_in_the_one_piece_bucket
+          __add_it_to_the_cha_cha_table
+        end
+
+        __allocate_the_cha_cha_table
+
+        __flush
+      end
+
+      def __flush
+
+        remove_instance_variable :@_entry_string_via_offset
+        len_etc = remove_instance_variable :@_length_of_longest_whatever
+        bx = remove_instance_variable :@_offset_via_sigil_box
+        remove_instance_variable :@_pieces_via_offset
+        items = remove_instance_variable :@_remote_items
+
+        _a = instance_variables
+        if _a.length.nonzero?
+          oops
+        end
+
+        # as courtesy, expose the sigils in an order that corresponds to
+        # the order the original items were received in, not our weird order
+
+        len = items.length
+        a = ::Array.new len
+        countdown = len
+
+        bx.each_pair do |sigil, d|
+          a[ d ] && oops
+          a[ d ] = sigil.freeze
+          countdown -= 1
+        end
+        a.freeze
+        countdown.zero? || oops
+
+        Result___.new a, len_etc, items
+      end
+
+      # -- E.
+
+      def __next_item_in_the_one_piece_bucket
+        send ( @_next ||= :__next_one_piece_initially )
+      end
+
+      def __next_one_piece_initially
+        _etc_for :@_one_piece_bucket
+      end
+
+      def __allocate_the_cha_cha_table
+
+        remove_instance_variable( :@_cha_cha_table ).each_pair do |sigil, d_a|
+
+          if 1 == d_a.length
+            @_offset_via_sigil_box.add sigil, d_a[0]
+            next
+          end
+
+          # "task" -> [tas]
+          # "tabular" -> [tab]
+
+          d_a.each do |d|
+            s_a = @_pieces_via_offset.fetch d
+            @_offset_via_sigil_box.add s_a.fetch( 0 )[ 0, 3 ], d
+          end
+        end
+
+        NIL
+      end
+
+      def __add_it_to_the_cha_cha_table
+
+        # what you can assume about the current item:
+        #   - its name is not simply a single 2 or 3 length piece
+        #   - its name is one piece
+
+        s_a = _current_pieces
+        1 == s_a.length || sanity
+        3 < s_a.first.length || sanity
+
+        sigil = s_a.first[ 0, 2 ]
+        if _this_sigil_is_already_being_used sigil
+          sigil = s_a.first[ 0, 3 ]
+        end
+
+        @_cha_cha_table.touch_array( sigil ).push @_current_item_offset
+        NIL
+      end
+
+      def __put_it_in_the_one_piece_bucket
+        @_one_piece_bucket.push @_current_item_offset ; nil
+      end
+
+      # -- D.
+
+      def __next_item_in_the_multiples_bucket
+        send ( @_next ||= :__next_multiple_initially )
+      end
+
+      def __next_multiple_initially
+        _etc_for :@_multiple_pieces_bucket
+      end
+
+      def __use_the_name_derived_from_each_first_character_of_each_piece
+
+        s_a = _current_pieces
+
+        sigil = s_a.reduce "" do |m, piece_s|
+          m << piece_s[ 0 ]
+        end
+
+        if _this_sigil_is_already_being_used sigil
+
+          # "code_metrics" => [cme]
+          # "code_molester" => [cmo] (but it's no longer there)
+          # "cm" -> [cm]
+
+          2 == s_a.length || never_been_kissed
+
+          buffer = s_a.fetch(0)[0]
+          buffer << s_a.fetch(1)[0,2]
+          sigil = buffer
+        end
+
+        _use_this_sigil sigil
+        NIL_
+      end
+
+      def __the_thing_ding_has_multiple_pieces
+        1 < _current_pieces.length
+      end
+
+      def __put_it_in_the_multiple_pieces_bucket
+        @_multiple_pieces_bucket.push @_current_item_offset ; nil
+      end
+
+      # -- (nearby shared)
+
+      def _etc_for ivar
+        _d_a = remove_instance_variable ivar
+        @_current_item_offset = nil
+        @_current_etc = Common_::Stream.via_nonsparse_array _d_a
+        @_next = :__next_etc_normally
+        send @_next
+      end
+
+      def __next_etc_normally
+        d = @_current_etc.gets
+        if d
+          @_current_item_offset = d ; true
         else
-          UNABLE_
+          remove_instance_variable :@_current_item_offset
+          remove_instance_variable :@_current_etc
+          remove_instance_variable :@_next ; false
         end
       end
 
-      def _resolve_conflicts_using m
+      # -- C.
 
-        conflicts = @_conflicts
+      def __current_thing_ding_is_exactly_two_or_three_characters_long
+        s_a = _current_pieces
+        1 == s_a.length and ( 2..3 ).include? s_a.first.length
+      end
 
-        conflicts.length.times do | d |
+      def __use_that
+        _use_this_sigil _current_entry_string
+        NIL_
+      end
 
-          _very_short_string = conflicts.fetch d
+      def _use_this_sigil sigil
+        @_offset_via_sigil_box.add sigil, @_current_item_offset
+        NIL
+      end
 
-          _yes = send m, _very_short_string
+      def _this_sigil_is_already_being_used sigil
+        @_offset_via_sigil_box.has_key sigil
+      end
 
-          if _yes
-            conflicts[ d ] = nil
+      # -- B.
+
+      def __next_item
+        send ( @_next_item ||= :__next_item_initially )
+      end
+
+      def __next_item_normally
+        x = @stemish_stream.gets
+        if x
+          @_current_item_offset += 1
+          @_current_item = x
+
+          s = @_current_item.entry_string.freeze
+          len = s.length
+          if @_length_of_longest_whatever < len
+            @_length_of_longest_whatever = len
           end
+          @_entry_string_via_offset.push s
+
+          _s_a = _current_entry_string.split( SPLITTER_RX___ ).freeze
+          @_pieces_via_offset.push _s_a
+          @_remote_items.push x
+          true
+        else
+          @_remote_items.freeze
+          remove_instance_variable :@_next_item
+          remove_instance_variable :@_current_item
+          remove_instance_variable :@_current_item_offset
+          remove_instance_variable :@stemish_stream
+          false
         end
-
-        conflicts.compact!
-
-        if conflicts.length.zero?
-          @_conflicts = nil
-        end
-
-        NIL_
       end
 
-      def _accept_new_very_short_strings made, bad_very_short_string
-
-        meda_a = @_box.remove bad_very_short_string
-
-        meda_a.each_with_index do | meda, d |
-
-          new_very_short = made.fetch d
-
-          meda.sigil = new_very_short  # or not mutate etc
-
-          @_box.add new_very_short, [ meda ]  # we gotta keep the structure :/
-        end
-        NIL_
+      def _current_entry_string
+        @_entry_string_via_offset.fetch @_current_item_offset
       end
 
-      class Sigilization___
 
-        def initialize sigil, pieces, up_x
+      SPLITTER_RX___ = /
+        _  # split on a plain old underscore
+        |
+        (?<=\D)(?=\d)  # split on the zero-width space between a not digit and a digit
+        |
+        (?<=\d)(?=\D)  # split on the zero-width space between a digit and a not digit
+      /x
 
-          @pieces = pieces
-          @sigil = sigil
-          @x = up_x
-        end
+      def __next_item_initially
+        @_current_item_offset = -1
+        @_current_item = nil
+        @_next_item = :__next_item_normally
+        send @_next_item
+      end
 
-        attr_writer :sigil
-
-        def entry_string
-          @x.entry_string
-        end
-
-        attr_reader(
-          :pieces,
-          :sigil,
-          :x,
-        )
+      def _current_pieces
+        @_pieces_via_offset.fetch @_current_item_offset
       end
     end
+
+    # ==
+
+    class Sigilization___
+
+      def initialize sigil, pieces, up_x
+
+        @pieces = pieces
+        @remote_item = up_x
+        @sigil = sigil
+        freeze
+      end
+
+      def entry_string
+        @remote_item.entry_string
+      end
+
+      attr_reader(
+        :pieces,
+        :remote_item,
+        :sigil,
+      )
+    end
+
+    # ==
+    # ==
   end
 end
+# :#history-B: as referenced, near full rewrite
 # #tombstone: #case-study: de-functionalize a short but unreadable tangle
---
+++ tmx/test/34-models/50-sigil_spec.rb
@@ -0,0 +1,221 @@
+require 'skylab/tmx'
+
+# (we needed this functionality for a re-factor that was happening
+# *while* the ecosystem was broken (the ecosystem installer) so we didn't
+# bother trying to have the full stack of testing tools for now..)
+
+
+# module Skylab::TMX::TestSupport
+
+class Skylab_TMX_OneOff_1
+
+  # describe "[tmx] models - sigil" do
+  begin
+
+    def self.it s, & p
+      @_a.push [ s, p ] ; nil
+    end
+    @_a = []
+
+    it "the result knows the length of the longest thing string" do
+
+      _d = _result.length_of_longest_entry_string
+      _d == 18 || fail
+    end
+
+    it "the items that had names 2 or 3 letters long are all as-is" do
+      _these %w(
+        arc arc
+        cm cm
+        git git
+        tmx tmx
+      )
+      # 4
+    end
+
+    it "the items that are multi. pieces long (not with number) are acronymized" do
+      _these %w(
+        beauty_salon bs
+        css_convert cc
+        doc_test dt
+        git_viz gv
+        search_and_replace sar
+        sub_tree st
+        tan_man tm
+        task_examples te
+        test_support ts
+      )
+      # 9
+    end
+
+    it "this one needed special handing to avoid an already taken name" do
+      _these %w(
+        code_metrics cme
+      )
+      # 1
+    end
+
+    it "the items that are multi. pieces long (yes number) are acronymized" do
+      _these %w(
+        bnf2treetop b2t
+        flex2treetop f2t
+        yacc2treetop y2t
+      )
+      # 3
+    end
+
+    it "these boring ones are boring" do
+      _these %w(
+        basic ba
+        brazen br
+        common co
+        cull cu
+        fields fi
+        human hu
+        myterm my
+        parse pa
+        permute pe
+        plugin pl
+        slicer sl
+        snag sn
+        system sy
+        treemap tr
+        zerk ze
+      )
+      # 15
+    end
+
+    it "but note we had to tiebreak these two" do
+      _these %w(
+        tabular tab
+        task tas
+      )
+      # 2
+    end
+
+    def _these s_a
+
+      idx = _this_index
+      d = -2
+      last = s_a.length - 2
+      while last != d
+        d += 2
+        before = s_a.fetch d
+        after = s_a.fetch d + 1
+        actual = idx.fetch before
+        actual == after || fail
+      end
+    end
+
+    -> do
+      x = nil ; once = -> me do
+        once = nil
+        rslt = me._result
+        items = rslt.instance_variable_get :@items
+        sigils = rslt.instance_variable_get :@sigils
+        h = {}
+        items.each_with_index do |my_item, d|
+          h[ my_item.entry_string ] = sigils.fetch d
+        end
+        x = h.freeze ; nil
+      end
+      define_method :_this_index do
+        once && once[ self ]
+        x
+      end
+    end.call
+
+    -> do
+      x = nil ; once = -> me do
+        once = nil
+        _long_list = me._this_one_long_list
+        _ = ::Skylab::Common::Stream.via_nonsparse_array( _long_list ).map_by do |s|
+          X_ms_EekWrap.new s
+        end
+        x = me._subject_module.via_stemish_stream( _ ) ; nil
+      end
+      define_method :_result do
+        once && once[ self ]
+        x
+      end
+    end.call
+
+    # the below "long list" is a snapshot of every current sidesystem at
+    # writing. this is *exactly* how the below "long list" was generated:
+    #
+    #   1) find . -type f -depth 2 -name scooby-doo | cut -d/ -f 2
+    #
+    #      (you have to substitute whatever is the value of
+    #      ::Skylab::TM::METADATA_FILENAME for "scooby-doo".)
+    #
+    #   2) manually add one certain sidesystem that is in development
+    #      in a different branch at writing (choreo massive).
+
+    -> do
+      a = %w(
+        arc
+        basic
+        beauty_salon
+        bnf2treetop
+        brazen
+        code_metrics
+        common
+        cm
+        css_convert
+        cull
+        doc_test
+        fields
+        flex2treetop
+        git
+        git_viz
+        human
+        myterm
+        parse
+        permute
+        plugin
+        search_and_replace
+        slicer
+        snag
+        sub_tree
+        system
+        tabular
+        tan_man
+        task
+        task_examples
+        test_support
+        tmx
+        treemap
+        yacc2treetop
+        zerk
+      ).freeze
+
+      34 == a.length || update_me
+
+      define_method :_this_one_long_list do
+        a
+      end
+    end.call
+
+    def _subject_module
+      ::Skylab::TMX::Models::Sigil
+    end
+
+    def self.__run_all_tests_
+
+      @_a.each do |_s, p|
+        new.instance_exec( & p )
+      end
+    end
+
+    class X_ms_EekWrap
+      def initialize s
+        @entry_string = s
+      end
+      attr_reader :entry_string
+    end
+
+    freeze  # (in case we accidentally try to re-use this same generic name elsehwere)
+  end
+end
+
+Skylab_TMX_OneOff_1.__run_all_tests_
