29578c06b791a3239aebfd633b152d7cd803c4a9
Mark Meves <mark.meves@gmail.com>
2019-02-28 12:05:41 -0500

    Provision new identifier integer pseudo-randomly (219)
    (times)
      02-27 01:14  begin looking for a document into which to write the
                   algo for provision. decide to write it into the asset.
            02:54  test written. zz
            11:32  play
            12:24  begin writing asset
      02-28 00:50  ok try really hard to close it off
            02:25  zz
            09:32  begin. eventually hit breakthrough of "gaps" abstraction
            10:27  passing all 6 high level allocation tests
            12:04  now

---
+++ kiss_rdb/magnetics_/provision_ID_randomly_via_identifiers.py
@@ -0,0 +1,179 @@
+"""SO...
+
+here we conceive of the collection as a numberspace (just a range) with each
+"spot" (integer) in the range either already occupied by an item or it's an
+open "slot" available for an item to be placed in it.
+
+our behavior for "new identifier provision" is that each time we have a new
+item to place in the collection, we chose a slot pseudo-randomly so that on
+balance the items are placed in to the space with a flat, even distribution.
+
+"why" is outside of this scope, but the short of it is that it's about
+avoiding smells:
+
+  - it's a smell to assume that your items will be yielded out in the same
+    order they were added in (more specifically it's a big liability).
+
+  - thought should be put into what "business sequence" means. one should
+    design and specify explicitly how to represent business sequence in
+    data; rather than it being an afterthought decided implicitly by the
+    datastore in its implementation detail.
+
+if you have 1,000 "slots" then the first number you allocate you say
+"give me a random number between 0 and 999". for the second identifier
+you provision, we say "give me a random number between 0 and 998". now,
+this second time you do *not* use *that* number as the new identifier..
+
+rather, you use that number (N) and say "the Nth open slot is the new
+identifier".
+
+in this ASCII visualization X'es are already occupied integers and
+"|"s are open slots:
+
+                  -      -      -      -      -      -      -
+                0 |    0 |    0 |    0 |    0 X    0 X    0 X
+                1 |    1 |    1 |    1 |    1 |    1 |    1 X
+                2 |    2 X    2 X    2 X    2 X    2 X    2 X
+                3 |    3 |    3 X    3 X    3 X    3 X    3 X
+                4 |    4 |    4 |    4 |    4 |    4 X    4 X
+                5 |    5 |    5 |    5 X    5 X    5 X    5 X
+                  -      -      -      -      -      -      -
+
+      size of pool:  6     5     4      3      2      1      0 (full)
+
+      randomly gen'd:  2     2     3      0      1      0
+
+      new integer is:    2     3     5      0      4      1
+
+at each step, we get a random number between 0 and N-1 given pool size N.
+in effect, walk along every open slot that random number number of times.
+whenever you stop, that integer in the space you are "standing on" is the
+newly provisioned integer for you.
+
+note:
+
+    - after each step (allocation) the pool is one smaller
+
+    - to generate the random number you have to know the pool size
+
+    - mapping from random number to provisioned identifier is not magic: you
+      have to know every occupied slot; i.e the whole existing allocated state.
+
+there's a variety of ways we considered implementing this, with things
+like B-trees or custom data structures so that we don't have to traverse
+the whole collection of identifiers twice. but MEH:
+"""
+
+
+def provision_integer(provisioned_integers, capacity, random_number_generator):
+    """DISCUSSION:
+
+    assume the already provisioned integers each fall within the range
+    between ZERO and (capacity minus one) INCLUSIVE.
+
+    there will be other ways that save on compute (maybe), but this way will
+    be easiest to code:
+
+      - determine the "pool size" by flattening ALL already provisioned
+        integers into one big array.
+
+      - send this pool size into the random number generator. (if the pool
+        size is one, the only possible random number generated is zero.)
+
+      - this pseudo-randomly generated number is the "slot offset". to get
+        from this to the newly provisioned integer, YOU HAVE TO COUNT the
+        open slots from the beginning...
+
+    keep searching for the slot by hopping along "gaps" and adding up how
+    many "slots" you've passed over until you go past the "slot offset"
+    determimed by the random number generated. once you do, "back up" to
+    the exact right slot/integer.
+    """
+
+    provisioned_integers_sanitized = __sanitized_provisioned_integers(
+            provisioned_integers, capacity)
+
+    count_already_provisioned = len(provisioned_integers_sanitized)
+
+    pool_size = capacity - count_already_provisioned
+
+    slot_offset = random_number_generator(pool_size)
+
+    total_slots_passed_over = 0
+
+    for cursor, w in _gaps_via(provisioned_integers_sanitized, capacity):
+
+        total_slots_passed_over += w
+
+        if slot_offset < total_slots_passed_over:
+
+            _back_up_this_far = total_slots_passed_over - slot_offset
+            _end = cursor + w
+
+            result = _end - _back_up_this_far
+            break
+
+    return result
+
+
+def _gaps_via(integers, capacity):
+    """a really useful abstraction for our purposes:
+
+    convert (reduce) a stream of integers into a stream of
+    *non-zero width gaps* between the integers:
+
+    for "--XX-X" yield ("--", "-")..
+    """
+
+    cursor = 0
+    for integer in integers:
+        distance = integer - cursor
+        if 0 < distance:
+            yield (cursor, distance)
+        cursor = integer + 1
+
+    distance = capacity - cursor
+    if 0 < distance:
+        yield (cursor, distance)
+
+
+def __sanitized_provisioned_integers(provisioned_integers, capacity):
+    """produce an array of integers from the argument while validating
+
+    so:
+      - collapse a collection that's a generator (so we know length)
+      - ensure that it came in ordered. (do not order it yourself.)
+      - IFF the collecton is non-empty:
+          - make sure its lowest (so beginning) integer is not less than zero.
+          - make sure its highest (so ending) integer is within the
+            boundary implied by `capacity`.
+    """
+
+    a = []
+
+    itr = iter(provisioned_integers)
+    for i in itr:  # once
+        previous_int = i
+        a.append(i)
+        break
+
+    for i in itr:
+        if previous_int >= i:
+            cover_me("provisioned integer stream is not in order")
+        previous_int = i
+        a.append(i)
+
+    if len(a):
+        if 0 > a[0]:
+            cover_me("lowest integer is too low")
+
+        if capacity <= a[-1]:
+            cover_me("highest integer is too high")
+
+    return a
+
+
+def cover_me(msg=None):
+    raise Exception('cover me' if msg is None else f'cover me: {msg}')
+
+# #born.
---
+++ kiss_rdb_test/test_400_magnetics/test_672_provision_new_identifier_randomly.py
@@ -0,0 +1,121 @@
+import _common_state  # noqa: F401
+import unittest
+
+
+class _CommonCase(unittest.TestCase):
+
+    def expect(self, before, after):
+        provisioned_before, length = _provisioned_via_bar(before)
+        provisioned_before = tuple(provisioned_before)  # make it resuable
+        new_int = _subject_module().provision_integer(
+                provisioned_integers=provisioned_before,
+                capacity=length,
+                random_number_generator=_random_number_generator,
+                )
+
+        expected_provisioned_after, _ = _provisioned_via_bar(after)
+        expected_provisioned_after = tuple(expected_provisioned_after)
+
+        actual_provisioned_after = provisioned_before + (new_int,)
+        actual_provisioned_after = sorted(actual_provisioned_after)
+
+        self.assertSequenceEqual(actual_provisioned_after, expected_provisioned_after)  # noqa: E501
+
+
+def _provisioned_via_bar(bar):
+
+    length = len(bar)
+    _ = (i for i in range(0, len(bar)) if _yes_no[bar[i]])
+    return _, length
+
+
+_yes_no = {
+        '-': False,
+        'X': True,
+        }
+
+
+class Case740_simplified(_CommonCase):
+
+    def test_07_load_module(self):
+        self.assertIsNotNone(_subject_module())
+
+    def test_21_from_6(self):
+        self.expect(
+            '------',
+            '--X---')
+
+    def test_36_from_5(self):
+        self.expect(
+            '--X---',
+            '--XX--')
+
+    def test_50_from_4(self):
+        self.expect(
+            '--XX--',
+            '--XX-X')
+
+    def test_64_from_3(self):
+        self.expect(
+            '--XX-X',
+            'X-XX-X')
+
+    def test_79_from_2(self):
+        self.expect(
+            'X-XX-X',
+            'X-XXXX')
+
+    def test_93_from_1(self):
+        self.expect(
+            'X-XXXX',
+            'XXXXXX')
+
+
+def _random_number_generator(pool_size):
+    """
+    this is our super-easy-to-read mock of a random number generator.
+
+    we imagine that our real-world provision of a random number generator
+    will go something like this:
+
+        import random
+        randrange = random.Random('some seed idk').randrange
+
+        def random_number_generator(pool_size):
+            return randrange(0, pool_size)
+
+        return random_number_generator
+
+    although the asset we are testing (our "system under test") is
+    fundamentally based around pseudo-randomness, in our test we want
+    opaque, explicit, easy-to-read determinancy.
+
+    we can get determinancy by seeding the random number generator with some
+    hard-coded constant value as we have done above. however, we want the
+    finer-grained control of being able to specify number-by-number what
+    number is generated at each step so that the number generation fits our
+    didactic story.
+    """
+
+    return _xx[pool_size]
+
+
+_xx = {
+    6: 2,
+    5: 2,
+    4: 3,
+    3: 0,
+    2: 1,
+    1: 0,
+    }
+
+
+def _subject_module():
+    from kiss_rdb.magnetics_ import provision_ID_randomly_via_identifiers as _
+    return _
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# #born.
