441a71b8fbd581890bca13f5c915147acb0435cc
Mark Meves <mark.meves@gmail.com>
2020-08-02 11:54:44 -0400

    Finish rough draft of dot2cytoscape (proof of concept)
    (times)
      07-27 09:36  begin intermediate representation for dot2cytoscape

--- pho/cli/dot2cytoscape.py
+++ pho/cli/dot2cytoscape.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import re
+
+
 def cli_for_production():
     import sys as o
     from os import environ
@@ -49,11 +52,26 @@ class CLI:
         return self._exitstatus
 
     def _execute_main(self):
+        import json
+
+        def write_dict(dct):
+            w(json.dumps(dct))
+
         w = self._stdout.write
+
         with open(self._input_path) as input_lines:
-            _ = _output_lines_via_input_lines(input_lines, self._listener)
-            for output_line in _:
-                w(output_line)
+            dcts = _output_dicts_via_input_lines(input_lines, self._listener)
+
+            for dct in dcts:
+                w('[')
+                write_dict(dct)
+                break
+
+            for dct in dcts:
+                w(',\n')
+                write_dict(dct)
+
+        w(']\n')
 
     def _parse_ARGV(self):
         a = []
@@ -108,14 +126,192 @@ def _line_writer_for(io):
     return w
 
 
-def _output_lines_via_input_lines(lines, listener):
+def _output_dicts_via_input_lines(lines, listener):
 
     ast = _AST_via_input_lines(lines, listener)
     if ast is None:
         return
 
-    yield 'line 1\n'
-    yield 'line 2\n'
+    next_node_ID = _IdentifierIncrementer('n')
+    next_edge_ID = _IdentifierIncrementer('e')
+
+    local_group_ID_via_remote_node_ID = {}
+    local_node_ID_via_remote_node_ID = {}
+
+    itr = _intermediate_representation_via_AST(ast)
+    typ, *rest = next(itr)
+
+    while 'group' == typ:
+        label, members = rest
+        local_node_ID = next_node_ID()
+        for remote_ID in members:
+            local_group_ID_via_remote_node_ID[remote_ID] = local_node_ID
+
+        yield _local_node(local_node_ID, label)
+        typ, *rest = next(itr)
+
+    while 'node' == typ:
+        remote_node_ID, *attrs = rest
+        local_node_ID = next_node_ID()
+
+        local_node_ID_via_remote_node_ID[remote_node_ID] = local_node_ID
+
+        parent_ID = local_group_ID_via_remote_node_ID.get(remote_node_ID)
+        specials = []
+
+        label = None
+        seen_filled = False
+        for k, v in attrs:
+            if 'label' == k:
+                label = v
+            elif 'style' == k:
+                assert('filled' == v)
+                specials.append(('done', True))
+                seen_filled = True
+            else:
+                assert('shape' == k)
+                # and ignore
+
+        if not seen_filled:
+            specials.append(('done', False))
+
+        if label is None:
+            label = ''
+
+        yield _local_node(local_node_ID, label, parent_ID, specials)
+
+        typ, *rest = next(itr)
+
+    while 'association' == typ:
+        remote_source_ID, remote_dest_ID = rest
+        local_source_ID = local_node_ID_via_remote_node_ID[remote_source_ID]
+        local_dest_ID = local_node_ID_via_remote_node_ID[remote_dest_ID]
+        yield _local_edge(next_edge_ID(), local_source_ID, local_dest_ID)
+        typ, *rest = next(itr)
+
+    assert 'done' == typ
+
+    for _ in itr:
+        assert(False)
+
+
+def _local_node(local_node_ID, label, parent_ID=None, specials=()):
+    data = {}
+    data['id'] = local_node_ID
+    if parent_ID is not None:
+        data['parent'] = parent_ID
+    data['label'] = label
+
+    if len(specials):
+        for k, v in specials:
+            data[k] = v
+
+    outer = {}
+    outer['group'] = 'nodes'
+    outer['data'] = data
+
+    return outer
+
+
+def _local_edge(local_edge_ID, local_source_ID, local_dest_ID):
+    data = {'id': local_edge_ID, 'source': local_source_ID, 'target': local_dest_ID}  # noqa: E501
+    return {'group': 'edges', 'data': data}
+
+
+class _IdentifierIncrementer:
+
+    def __init__(self, head):
+        self._current_integer = 0
+        self._head = head
+
+    def __call__(self):
+        self._current_integer += 1
+        return f'{self._head}{self._current_integer}'
+
+
+def _intermediate_representation_via_AST(ast):
+
+    def on_node_forward_declaration(re):
+        # these occur so graph viz places them in certain places. ignore..
+        return ()
+
+    def on_association_line(md):
+        # we don't emit associations until the end
+        associations_cached.append(md.groups())
+        return ()
+
+    associations_cached = []
+
+    def on_subgraph(dct):
+        line = dct['subgraph_label_line'][0]  # ..
+        encoded_label = re.match('^  label="([^"]+)"$', line)[1]
+        _ = tuple(md[1] for md in dct.get('node_forward_declaration', ()))
+        yield 'group', encoded_label, _
+
+    function_via_type = {
+            'node_forward_declaration': on_node_forward_declaration,
+            'subgraph': on_subgraph,
+            'association_line': on_association_line,
+            }
+
+    for ihp in ast['interesting_head_phrase']:
+        typ, data = ihp['interesting_head_item']
+        for sexp in function_via_type[typ](data):
+            yield sexp
+
+    # ==
+
+    for np in ast['node_phrase']:
+        yield tuple(_node_tuple_parts(* np['node_phrase_body']))
+
+    for lhs, rhs in associations_cached:
+        yield 'association', lhs, rhs
+
+    for ap in ast['association_phrase']:
+        yield 'association', * ap['association_line'].groups()
+
+    yield ('done',)
+
+
+def _node_tuple_parts(typ, data):
+    yield 'node'
+    if 'single_line_node' == typ:
+        yield data[1]
+        for k, x in _name_value_big_hack(data[2]):
+            yield k, x
+        return
+
+    assert('multi_line_node' == typ)
+    yield data['open_node_line'][1]
+    ss = [data['open_node_line'][2]]
+    for qq in data.get('not_closing_node_line', ()):
+        xx()
+    ss.append(data['close_node_line'][1])
+    big_string = ''.join(ss)
+    for k, x in _name_value_big_hack(big_string):
+        yield k, x
+
+
+def _name_value_big_hack(string):
+    leng = len(string)
+    i = 0
+    while True:
+        md = _attr_head.match(string, i)
+        key = md[1]
+        i = md.span()[1]
+        md = _attr_RHS.match(string, i)
+        value = s.replace('\\n', '\n') if (s := md[1]) else md[2]
+        yield key, value
+        i = md.span()[1]
+        if leng == i:
+            break
+        md = _one_or_more_space.match(string, i)
+        i = md.span()[1]
+
+
+_attr_head = re.compile('([a-z_]+)=')
+_attr_RHS = re.compile('"([^"]*)"|([^ ]+)')
+_one_or_more_space = re.compile('[ ]+')
 
 
 def _AST_via_input_lines(lines, listener):  # #testpoint
@@ -247,9 +443,9 @@ def _define_grammar(g):
     ))
 
     define('multi_line_node', sequence(
-        ('one', 'open_node_line'),
-        ('zero_or_more', 'not_closing_node_line'),
-        ('one', 'close_node_line'),
+        ('one', 'open_node_line', 'keep'),
+        ('zero_or_more', 'not_closing_node_line', 'keep'),
+        ('one', 'close_node_line', 'keep'),
     ))
 
     define('open_node_line', regex(f'^({n})\\[([^\\]]*)$'))
