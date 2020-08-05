e5c6f4ea76bee37973ae11b4e7690614288ff3eb
Mark Meves <mark.meves@gmail.com>
2020-08-05 12:52:06 -0400

    Merge-in electron-forge-generated app
    The most recent electron tutorial we are following (documented) seems
    to like electron-forge (especially for deployment) so we follow suit
    
    - Experimental: imagine an “annotated entity revisions”
    
    (times)
      08-02 11:55  start studying electron from scratch
      08-04 10:56  ok try tmx notecards (electron app) again
            12:04  quick nap
            16:20  resume
            17:00  zumba, dinner, anime
      08-05 09:40  resume (lining up stuff for merge of new over old)
            10:49  (new, hand-merged app ran successfully)
            12:54  finished merging

--- pho/pho-gui/src/index.js
+++ pho/pho-gui/src/index.js
@@ -27,15 +27,14 @@ pyshell.end((err, code, signal) => {
   console.log('finished with thing.');
 });
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
 
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-function createWindow () {
+const createWindow = () => {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -44,44 +43,39 @@ function createWindow () {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('src/index.html');
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  });
-}
+  // (on window closed, we used to dereference a global before #history-A.1)
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// code. You can also put them in separate files and import them here.
 /*
+# #history-A.1: merge-in electron-forge-generated starter app
 # #born.
 */
