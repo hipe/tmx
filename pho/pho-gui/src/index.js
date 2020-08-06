2af34d4bf7a9e5330d4cafb85018b2417fc53559
Mark Meves <mark.meves@gmail.com>
2020-08-06 17:23:46 -0400

    Remove screen-recording-tutorial-specific code
    Use strict. (Bringing back this idea from the "todo" tutorial).
    
    Experimenting with Bulma’s column layout for our UI for now.
    
    (times)
      15:50  after nap lets try again. extremely lacking in motivation
      16:53  decide we are going "get random entity"

--- pho/pho-gui/src/index.js
+++ pho/pho-gui/src/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { app, BrowserWindow } = require('electron');
 
 const { PythonShell } = require('python-shell');
--- pho/pho-gui/src/render.js
+++ pho/pho-gui/src/render.js
@@ -1,106 +1,29 @@
-const { desktopCapturer, remote } = require('electron');
-const { writeFile } = require('fs');
-const { dialog, Menu } = remote;
-
-// Global state
-let mediaRecorder;  // to capture footage
-const recordedChunks = [];
+'use strict';
 
 // Buttons
-const videoElement = document.querySelector('video');
+const randomBtn = document.getElementById('randomBtn');
 
-const startBtn = document.getElementById('startBtn');
-startBtn.onclick = e => {
-  mediaRecorder.start();
-  startBtn.classList.add('is-danger');
-  startBtn.innerText = 'Recording';
+(btn => {
+
+  // temporary state hack, just for dumb demo
+  let isOn = false;
+  let prevText = null;
+
+  btn.onclick = e =>  {
+    if (isOn) {
+      isOn = false;
+      btn.classList.remove('is-danger');
+      btn.innerText = prevText;
+      return;
+    }
+    isOn = true;
+    btn.classList.add('is-danger');
+    prevText = btn.innerText;
+    btn.innerText = 'Retrieving';
 };
-
-const stopBtn = document.getElementById('stopBtn');
-stopBtn.onclick = e => {
-  mediaRecorder.stop();
-  startBtn.classList.remove('is-danger');
-  startBtn.innerText = 'Start';
-}
-
-const videoSelectBtn = document.getElementById('videoSelectBtn');
-videoSelectBtn.onclick = getVideoSources;
-
-
-// Get the available video sources
-async function getVideoSources() {
-
-  const inputSources = await desktopCapturer.getSources({
-    types: ['window', 'screen'],
-  });
-
-  const videoOptionsMenu = Menu.buildFromTemplate(
-    inputSources.map(source => {
-      return {
-        label: source.name,
-        click: () => selectSource(source)
-      };
-    })
-  );
-
-  videoOptionsMenu.popup();
-}
-
-// Change the video source to record
-async function selectSource(source) {
-  videoSelectBtn.innerText = source.name;
-
-  const constraints = {
-    audio: false,
-    video: {
-      mandatory: {
-        chromeMediaSource: 'desktop',
-        chromeMediaSourceId: source.id,
-      },
-    },
-  };
-
-  console.log("here is your media source ID: " + source.id);
-
-  // Create a stream
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
-
-  // Preview the source in a video element
-  videoElement.srcObject = stream;
-  videoElement.play();
-
-  // Create the Media Recorder
-  const options = { mimeType: 'video/webm; codecs=vp9' };
-  mediaRecorder = new MediaRecorder(stream, options);
-
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.onstop = handleStop;
-}
-
-// Captures all recorded chunks
-function handleDataAvailable(e) {
-  console.log('video data available');
-  recordedChunks.push(e.data);
-}
-
-async function handleStop(e) {
-  const blob = new Blob(recordedChunks, {
-    type: 'video/webm; codecs=vp9',
-  });
-
-  const buffer = Buffer.from(await blob.arrayBuffer());
-
-  const { filePath } = await dialog.showSaveDialog({
-    buttonLabel: 'Save video',
-    defaultPath: `vid-${Date.now()}.webm`,
-  });
-
-  if (filePath) {
-    console.log(filePath);
-    writeFile(filePath, buffer, () => console.log('wrote video file'));
-  }
-}
+})(randomBtn);
 
 /*
+ * #history-A.1: remove screen recording tutorial code
  * #born (from tutorial)
  */
