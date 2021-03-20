9a634dd4fcdeab792a814c5636d0a29a3cc4aa9a
Mark Meves <mark.meves@gmail.com>
2021-03-20 15:08:17 -0400

    wip: add images and style to “hello world”
    Amazing.

--- grep_dump/rollup.config.js
+++ grep_dump/rollup.config.js
@@ -4,6 +4,10 @@ import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import image from '@rollup/plugin-image';
+import postcss from 'rollup-plugin-postcss';
+
+
 export default {
   input: "src/index.js",
   output: {
@@ -12,6 +16,10 @@ export default {
     sourcemap: true,
   },
   plugins: [
+    image(),
+    postcss({
+      extensions: [".css"],
+    }),
     nodeResolve({
       extensions: [".js"],
     }),
