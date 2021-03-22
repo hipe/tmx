1fa06b223c29bccbb9b34042054cc6a6e06d83cb
Mark Meves <mark.meves@gmail.com>
2021-03-22 07:21:18 -0400

    wip: achieve tic-tac-toe (step 1) using Rollup
    This is a "waypoint" (a "save").
    
    We are about to switch to *not* using Rollup (for now) so that we
    follow the "orthodoxy" of the "official" tutorial from the React
    documentation (that we must treat as orthodoxy while we don't know
    what we're doing).
    
    But wanted to save this state for posterity for if/when we change
    back to rollup, so we can get an idea for how it was working.

--- grep_dump/src/index.js
+++ grep_dump/src/index.js
@@ -1,9 +1,78 @@
+// (lines from the previous hello world are commented out and marked "#here1")
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';  #here1
+import './index.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+
+  renderSquare(i) {
+    return <Square />;
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+// ========================================
+
+// ReactDOM.render(<App />, document.querySelector('#root'));   # #here1
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
 
 /*
+# #history-B.4: begin tic tac toe
 # #born
 */
