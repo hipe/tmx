fd17762849d024ab80ac3b6d41126971c58d8a8e
Mark Meves <mark.meves@gmail.com>
2021-03-25 23:33:30 -0400

    wip: add interactivity: click makes X now
    (times)
      03-24 14:22  zz
            23:57  resumed
      03-25 00:54  getting sleepy
            23:21  ok let's go
    .

--- grep_dump/src/index.js
+++ grep_dump/src/index.js
@@ -19,6 +19,13 @@ class Game extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   render() {
     const status = 'Next player: X';
 
@@ -45,15 +52,29 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 }
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
