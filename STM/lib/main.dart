1bd9786c7d638d73c7542f8bcbdc86e39b3f7309
Mark Meves <mark.meves@gmail.com>
2023-02-22 09:07:30 -0500

    moment(STM): Add button (from memory) for step 4

--- STM/lib/main.dart
+++ STM/lib/main.dart
@@ -37,8 +37,14 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random idea?'),
+          Text(appState.current.asPascalCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
