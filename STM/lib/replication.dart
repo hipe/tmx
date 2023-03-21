fadf3996080c08657127a31e50e302dcdc67f0a6
Mark Meves <mark.meves@gmail.com>
2023-03-21 12:12:31 -0400

    snapshot(STM): Capture nonworking (on Linux) sqflite (!)
    The purpose of this commit is to "prove" that sqflite
    throws errors on the didactic, minimal example of connecting.
    (We could use this snapshot against future versions of flutter
    to check if they've rolled linux desktop support into it yet.)

--- STM/lib/replication.dart
+++ STM/lib/replication.dart
@@ -1,6 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
 
 class ThingDing {
   int value = 0;
+
+  Future<void> tryThis() async {
+    print('yes beginning');
+    WidgetsFlutterBinding.ensureInitialized();
+    final database = openDatabase(
+      join(await getDatabasesPath(), 'some_derta_berse.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE erase_me(id INTEGER PRIMARY KEY, name TEXT)',
+        );
+      },
+      version: 1,  // makes the onCreate get called
+    );
+    print('hello did it work?');
+  }
 
   void increment() => value++;
 
--- STM/test/replication_test.dart
+++ STM/test/replication_test.dart
@@ -2,7 +2,16 @@ import 'package:stm/replication.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('XXxx', () {
+
+  test('TEST TWO', () async {
+    print('ok starting async test');
+    final ting = ThingDing();
+    await ting.tryThis();
+    print('ok wow finished!');
+  });
+
+
+  test('TEST ONE', () {
     final ting = ThingDing();
     ting.increment();
     expect(ting.value, 1);
